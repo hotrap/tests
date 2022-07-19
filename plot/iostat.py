bae2d164eae338c0fb94a8d02c1851477baf0df6
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2022-07-19 18:21:31 +0800

    plot smoothing

--- plot/iostat.py
+++ plot/iostat.py
@@ -16,14 +16,28 @@ mpl.rcParams.update({
     })  # 设置全局字体
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
-if len(sys.argv) != 2:
-	print('Usage: ' + sys.argv[0] + ' dir')
+if len(sys.argv) != 3:
+	print('Usage: ' + sys.argv[0] + ' dir mean_step')
 	exit()
 
 d = sys.argv[1]
-iostat = pd.read_table(d + '/iostat.sh.txt', delim_whitespace=True)
+mean_step = int(sys.argv[2])
+iostat_raw = pd.read_table(d + '/iostat.sh.txt', delim_whitespace=True)
+iostat_raw['Time(Seconds)'] = (iostat_raw['Timestamp(ns)'] - iostat_raw['Timestamp(ns)'][0]) / 1e9
 
-iostat['Time(Seconds)'] = (iostat['Timestamp(ns)'] - iostat['Timestamp(ns)'][0]) / 1e9
+def range_mean(raw, start, end):
+	ret = raw[['sd_tps', 'cd_tps', 'sd_kB_read/s', 'sd_kB_wrtn/s', 'cd_kB_read/s', 'cd_kB_wrtn/s']][start:end].mean()
+	ret['Time(Seconds)'] = raw['Time(Seconds)'][start]
+	return ret
+
+iostat = pd.DataFrame()
+i = 0
+while i + mean_step < len(iostat_raw):
+	iostat = pd.concat([iostat, pd.DataFrame(range_mean(iostat_raw, i, i + mean_step)).T], ignore_index=True)
+	i += mean_step
+if i != len(iostat_raw):
+	iostat = pd.concat([iostat, pd.DataFrame(range_mean(iostat_raw, i, len(iostat_raw))).T], ignore_index=True)
+
 plt.plot(iostat['Time(Seconds)'], iostat['sd_tps'])
 plt.plot(iostat['Time(Seconds)'], iostat['cd_tps'])
 plt.legend(['SD', 'CD'], prop={'size': fontsize})
