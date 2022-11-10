64a63f8a7efefe0d0dcdfa8a395fc903f4ad6c28
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2022-11-10 20:36:25 +0800

    Update

--- exe-while.sh
+++ exe-while.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ ! $2 ]; then
 	echo Usage: $0 command output-dir
+	exit
+fi
+
+if [ ! -d $2 ]; then
+	mkdir -p $2
+fi
+
+if [ "$(ls -A $2)" ]; then
+	echo $2 is not empty!
 	exit
 fi
 
