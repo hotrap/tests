cdafa31947e342105aa810dd40712e1d6f12a9a9
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2022-12-16 13:51:02 +0800

    Fix interleaving two lists

--- helper/remap-hotter-closer.py
+++ helper/remap-hotter-closer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import sys
-import os
-import shutil
+from itertools import chain, zip_longest
 
 if len(sys.argv) != 2:
 	print('Usage: ' + sys.argv[0] + ' pivot-ratio')
@@ -20,32 +19,36 @@ for key in sys.stdin:
 keys_sorted = sorted(cnt)
 keys_sorted_by_cnt = sorted(cnt.items(), key = lambda item: item[1], reverse=True)
 assert(len(keys_sorted) == len(keys_sorted_by_cnt))
-n = len(keys_sorted)
-if n == 0:
+n_unique_keys = len(keys_sorted)
+if n_unique_keys == 0:
 	exit(0)
-pivot = int(n * pivot_ratio)
-assert(0 <= pivot and pivot < n)
+pivot = int(n_unique_keys * pivot_ratio)
+assert(0 <= pivot and pivot < n_unique_keys)
 
-n1 = min(pivot, n - pivot)
+n1 = min(pivot, n_unique_keys - pivot)
 l1 = keys_sorted[pivot-n1:pivot][::-1]
 l2 = keys_sorted[pivot:pivot+n1]
 # https://stackoverflow.com/questions/7946798/interleave-multiple-lists-of-the-same-length-in-python
-keys_closest = [ val for tup in zip(l1, l2) for val in tup ]
+def interleave(l1, l2):
+    return [x for x in chain.from_iterable(zip_longest(l1, l2)) if x is not None]
+keys_closest = interleave(l1, l2)
+assert(len(keys_closest) == len(l1) + len(l2))
 if pivot > n1:
-	assert(pivot + n1 == n)
+	assert(pivot + n1 == n_unique_keys)
 	p2 = (pivot - n1) // 2
 	l1 = keys_sorted[0:p2]
 	l2 = keys_sorted[p2:pivot - n1][::-1]
 else:
 	assert(pivot - n1 == 0)
 	base = pivot + n1
-	p2 = base + (n - base) // 2
+	p2 = base + (n_unique_keys - base) // 2
 	l1 = keys_sorted[base:p2]
-	l2 = keys_sorted[p2:n][::-1]
-keys_closest.extend([ val for tup in zip(l1, l2) for val in tup ])
+	l2 = keys_sorted[p2:n_unique_keys][::-1]
+keys_closest.extend(interleave(l1, l2))
+assert(len(keys_closest) == n_unique_keys)
 
 remap = {}
-for i in range(0, n):
+for i in range(0, n_unique_keys):
 	remap[keys_sorted_by_cnt[i][0]] = keys_closest[i]
 for item in remap.items():
 	print(*item)
