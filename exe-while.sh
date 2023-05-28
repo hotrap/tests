dd150b3eacd59b7d2aad49eb88bb743d5b3d7bfd
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-05-28 16:42:16 +0800

    Allow passing arguments to command in exe-while.sh

--- exe-while.sh
+++ exe-while.sh
@@ -3,19 +3,19 @@
 set -e
 
 if [ ! $2 ]; then
-	echo Usage: $0 command output-dir
+	echo Usage: $0 output-dir command [[args]]
 	exit
 fi
 
-if [ ! -d $2 ]; then
-	mkdir -p $2
+if [ ! -d $1 ]; then
+	mkdir -p $1
 fi
 
-if [ "$(ls -A $2)" ]; then
-	echo $2 is not empty!
+if [ "$(ls -A $1)" ]; then
+	echo $1 is not empty!
 	exit
 fi
 
-PID=$(set -m; ./periodic-exe.sh $2 > /dev/null & echo $!)
-$1
+PID=$(set -m; ./periodic-exe.sh $1 > /dev/null & echo $!)
+$2 ${@:3}
 kill $PID
