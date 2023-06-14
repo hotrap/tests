a088216d3399679c5f2bd118e656672b4f3129f1
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-06-14 16:48:53 +0800

    Fix exit code of exe-while.sh

--- exe-while.sh
+++ exe-while.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ ! $2 ]; then
 	echo Usage: $0 output-dir command [[args]]
-	exit
+	exit 1
 fi
 
 if [ ! -d $1 ]; then
@@ -13,9 +13,9 @@ fi
 
 if [ "$(ls -A $1)" ]; then
 	echo $1 is not empty!
-	exit
+	exit 1
 fi
 
 PID=$(set -m; ./periodic-exe.sh $1 > /dev/null & echo $!)
-$2 ${@:3}
+$2 "${@:3}"
 kill $PID
