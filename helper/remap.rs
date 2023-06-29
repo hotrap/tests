973de83f1e3b41f4a8b1b877b29275942bb77450
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-06-29 11:39:55 +0800

    Make remap support plain trace format

--- helper/remap.rs
+++ helper/remap.rs
@@ -1,46 +1,106 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
-use std::collections::HashMap;
+
+#[derive(PartialEq)]
+enum Format {
+    Plain,
+    YCSB,
+}
+
+/// Read all bytes into buf until a whitespace byte or EOF is reached.
+/// The whitespace byte will also be appended to buf.
+fn read_until_whitespace<R: BufRead>(
+    mut reader: R,
+    buf: &mut Vec<u8>,
+) -> io::Result<()> {
+    'outer: loop {
+        let b = reader.fill_buf()?;
+        if b.is_empty() {
+            break;
+        }
+        for (i, c) in b.iter().enumerate() {
+            buf.push(*c);
+            if c.is_ascii_whitespace() {
+                reader.consume(i + 1);
+                break 'outer;
+            }
+        }
+        let len = b.len();
+        drop(b);
+        reader.consume(len);
+    }
+    Ok(())
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("Usage: {} remap-file", args[0]);
+    if args.len() != 3 {
+        println!("Usage: {} format remap-file", args[0]);
         return;
     }
-    let remap_file = File::open(&args[1]).unwrap();
+    let format = &args[1];
+    let remap_file = File::open(&args[2]).unwrap();
+
+    let format = match format.as_str() {
+        "plain" => Format::Plain,
+        "ycsb" => Format::YCSB,
+        _ => panic!("Unsupported format {}", format),
+    };
     let mut remap_reader = BufReader::new(remap_file);
     let mut remap = HashMap::<Vec<u8>, Vec<u8>>::new();
     loop {
         let mut k1 = Vec::new();
-        if remap_reader.read_until(b' ', &mut k1).unwrap() == 0 {
+        read_until_whitespace(&mut remap_reader, &mut k1).unwrap();
+        if k1.len() == 0 {
             break;
         }
+        assert_eq!(*k1.last().unwrap(), b' ');
+        k1.pop();
         let mut k2 = Vec::new();
-        assert!(remap_reader.read_until(b'\n', &mut k2).unwrap() != 0);
-        assert!(*k2.last().unwrap() == b'\n');
-        k2.pop();
+        read_until_whitespace(&mut remap_reader, &mut k2).unwrap();
+        assert!(!k2.is_empty());
+        if k2.last().unwrap().is_ascii_whitespace() {
+            assert!(k2.pop().unwrap() == b'\n');
+        }
         remap.insert(k1, k2);
     }
     let mut cin = BufReader::new(io::stdin());
     loop {
         let mut line = Vec::new();
         // Operation
-        if cin.read_until(b' ', &mut line).unwrap() == 0 {
+        read_until_whitespace(&mut cin, &mut line).unwrap();
+        if line.len() == 0 {
             break;
         }
-        // usertable
-        assert!(cin.read_until(b' ', &mut line).unwrap() != 0);
+        assert_eq!(*line.last().unwrap(), b' ');
+        if format == Format::YCSB {
+            // usertable
+            let ori_len = line.len();
+            read_until_whitespace(&mut cin, &mut line).unwrap();
+            assert!(line.len() != ori_len);
+        }
         let mut key = Vec::new();
-        assert!(cin.read_until(b' ', &mut key).unwrap() != 0);
+        read_until_whitespace(&mut cin, &mut key).unwrap();
+        assert!(key.len() != 0);
+        let line_ends = if key.last().unwrap().is_ascii_whitespace() {
+            key.pop().unwrap() == b'\n'
+        } else {
+            false
+        };
         match remap.get(&key) {
             Some(new_key) => {
                 line.extend_from_slice(new_key);
-                line.push(b' ');
-            },
+            }
             None => line.append(&mut key),
         }
-        assert!(cin.read_until(b'\n', &mut line).unwrap() != 0);
+        if line_ends {
+            line.push(b'\n');
+        } else {
+            line.push(b' ');
+            assert!(cin.read_until(b'\n', &mut line).unwrap() != 0);
+        }
         print!("{}", String::from_utf8(line).unwrap());
     }
 }
