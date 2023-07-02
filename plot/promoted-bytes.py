128defec62c489eab76a03eb38bef135dc90ad7e
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-07-02 18:52:03 +0800

    promoted-get-bytes

--- plot/promoted-bytes.py
+++ plot/promoted-bytes.py
@@ -21,15 +21,21 @@ mpl.rcParams.update({
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 d = sys.argv[1]
-promoted_bytes_raw = pd.read_table(d + '/promoted-iter-bytes', delim_whitespace=True)
-time = (promoted_bytes_raw['Timestamp(ns)'] - promoted_bytes_raw['Timestamp(ns)'][0]) / 1e9
-promoted_bytes = promoted_bytes_raw['num-bytes']
+promoted_iter_raw = pd.read_table(d + '/promoted-iter-bytes', delim_whitespace=True)
+promoted_iter_time = (promoted_iter_raw['Timestamp(ns)'] - promoted_iter_raw['Timestamp(ns)'][0]) / 1e9
+promoted_iter_bytes = promoted_iter_raw['num-bytes']
+
+promoted_get_raw = pd.read_table(d + '/promoted-get-bytes', delim_whitespace=True)
+promoted_get_time = (promoted_get_raw['Timestamp(ns)'] - promoted_get_raw['Timestamp(ns)'][0]) / 1e9
+promoted_get_bytes = promoted_get_raw['num-bytes']
 
 plot_dir = d + '/plot'
 if not os.path.exists(plot_dir):
 	os.system('mkdir -p ' + plot_dir)
 pdf_path = plot_dir + '/promoted-bytes.pdf'
-plt.plot(time, promoted_bytes)
+plt.plot(promoted_iter_time, promoted_iter_bytes)
+plt.plot(promoted_get_time, promoted_get_bytes)
+plt.legend(['By iter', 'By get'], prop={'size': fontsize})
 plt.xlabel('Time (Seconds)', fontdict=fonten)
 plt.ylabel('Promoted bytes', fontdict=fonten)
 plt.savefig(pdf_path)
