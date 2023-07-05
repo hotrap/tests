b62dbbee371be97c4dc460a4b1eaafd4be05ac09
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-07-05 13:11:43 +0800

    Fix comments

--- hrtg/shifting-hotspot.sh
+++ hrtg/shifting-hotspot.sh
@@ -1,6 +1,6 @@
 set -e
 workload_file=../workloads/config_read_0.5_insert_0.5_shifting_hotspot_hotspotdatafraction_0.1_hotspotopnfraction_0.9_2e7_2e7
-# cache_size=0, SD=4GB
+# SD=4GB
 DIR=../../data/shifting-hotspot/rocksdb
 ./rocksdb.sh 4GB $workload_file $DIR
 mkdir -p $DIR/plot/
@@ -9,7 +9,7 @@ mkdir -p $DIR/plot/
 ../plot/throughput.py $DIR 1 &
 ../plot/latency.py < $DIR/latency > $DIR/plot/latency &
 wait
-# hit_count, cache_size=0, delta=2GB, SD=4GB, kAccurateHotSizePromotionSize
+# hit_count, delta=2GB, SD=4GB, kAccurateHotSizePromotionSize
 DIR=../../data/shifting-hotspot/hotrap
 ./hotrap.sh 4GB 2GB $workload_file $DIR
 mkdir -p $DIR/plot/
