23e8a2ff4842bd3f3c6e4a9f05a76cf5a209862c
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-07-31 22:02:35 +0800

    First kvexe then calculate occurrences

--- hrtg/hotrap.sh
+++ hrtg/hotrap.sh
@@ -22,9 +22,9 @@ fi
 kvexe_dir=../../kvexe/build/Release
 # hit_count, kAccurateHotSizePromotionSize
 ulimit -n 100000
+../helper/exe-while.sh $4 bash -c "set -e; set -o pipefail; ../../trace-generator/target/release/trace-generator $3 | $kvexe_dir/rocksdb-kvexe --cleanup --format=plain --compaction_pri=6 --max_hot_set_size=$max_hot_set_size --switches=0xd --num_threads=$num_threads --db_path=$HOME/testdb/db/ --db_paths=\"{{$HOME/testdb/sd,$sd_size},{$HOME/testdb/cd,100000000000}}\" --viscnts_path=$HOME/testdb/viscnts 2>> $4/log.txt"
 occurrences=$(mktemp)
 ../../trace-generator/target/release/trace-generator $3 | awk '{if ($1 == "READ") print $2}' | ../helper/bin/occurrences > $occurrences
-../helper/exe-while.sh $4 bash -c "set -e; set -o pipefail; ../../trace-generator/target/release/trace-generator $3 | $kvexe_dir/rocksdb-kvexe --cleanup --format=plain --compaction_pri=6 --max_hot_set_size=$max_hot_set_size --switches=0xd --num_threads=$num_threads --db_path=$HOME/testdb/db/ --db_paths=\"{{$HOME/testdb/sd,$sd_size},{$HOME/testdb/cd,100000000000}}\" --viscnts_path=$HOME/testdb/viscnts 2>> $4/log.txt"
 sort -nk2 -r $occurrences > $4/occurrences
 rm $occurrences
 bash ../helper/hotrap-data.sh "$4"
