4028cb27bc88af743588f7ca0e66c7bd8a094698
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-07-31 22:01:52 +0800

    Fix rocksdb-data.sh

--- helper/rocksdb-data.sh
+++ helper/rocksdb-data.sh
@@ -8,6 +8,7 @@ cd $(dirname $0)
 cd ../../testdb
 du -sh db/ sd/ cd/ >> "$DIR"/log.txt
 mv db/{rocksdb-stats.txt,progress,latency,cpu} "$DIR"/
-if [ -f db/ans_1 ]; then
-	mv db/ans_* "$DIR"/
+cd db
+if [ -f ans_0 ]; then
+	sha256sum ans_* > "$DIR"/ans.sha256
 fi
