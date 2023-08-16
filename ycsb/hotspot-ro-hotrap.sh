a9098d8b43a070567072a49f23b8be493855da57
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-08-16 18:30:56 +0800

    Split hotspot-ro.sh

--- ycsb/hotspot-ro-hotrap.sh
+++ ycsb/hotspot-ro-hotrap.sh
@@ -5,19 +5,10 @@ DIR=../../data/hotspot-ro/hotrap
 ./hotrap.sh 4GB 2GB $workload_file $DIR
 mkdir -p $DIR/plot/
 ../plot/du.py $DIR &
-../plot/ops.py $DIR 50 &
-../plot/tps.py $DIR 100 &
-../plot/throughput.py $DIR 200 &
+../plot/ops.py $DIR 10 &
+../plot/tps.py $DIR 10 &
+../plot/throughput.py $DIR 100 &
 ../plot/hit.py $DIR &
 ../plot/promoted-bytes.py $DIR &
-../plot/latency.py < $DIR/latency > $DIR/plot/latency &
-wait
-# SD=4GB
-DIR=../../data/hotspot-ro/rocksdb
-./rocksdb.sh 4GB $workload_file $DIR
-mkdir -p $DIR/plot/
-../plot/ops.py $DIR 50 &
-../plot/tps.py $DIR 50 &
-../plot/throughput.py $DIR 100 &
 ../plot/latency.py < $DIR/latency > $DIR/plot/latency &
 wait
