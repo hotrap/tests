c29d21db8dd8bf74413335a8f2d3525b71c59900
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-08-22 19:36:01 +0800

    Update

--- ycsb/hotspot-ro-hotrap.sh
+++ ycsb/hotspot-ro-hotrap.sh
@@ -1,7 +1,7 @@
 set -e
 workload_file=../workloads/workload_2e7_2e7_read_1_hotspot_hotspotdatafraction_0.1_hotspotopnfraction_0.9
 # hit_count, delta=2GB, SD=4GB, kAccurateHotSizePromotionSize
-DIR=../../data/hotspot-ro/hotrap
+DIR=../../data/hotspot-ro-hotrap
 ./hotrap.sh 4GB 2GB $workload_file $DIR
 mkdir -p $DIR/plot/
 ../plot/du.py $DIR &
--- ycsb/hotspot-ro-rocksdb.sh
+++ ycsb/hotspot-ro-rocksdb.sh
@@ -1,7 +1,7 @@
 set -e
 workload_file=../workloads/workload_2e7_2e7_read_1_hotspot_hotspotdatafraction_0.1_hotspotopnfraction_0.9
 # SD=4GB
-DIR=../../data/hotspot-ro/rocksdb
+DIR=../../data/hotspot-ro-rocksdb
 ./rocksdb.sh 4GB $workload_file $DIR
 mkdir -p $DIR/plot/
 ../plot/ops.py $DIR 50 &
