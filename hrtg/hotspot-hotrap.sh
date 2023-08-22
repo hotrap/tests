dcacf1586bddbd29657d12ef57689f41b8c22546
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-08-22 19:41:43 +0800

    Split hotspot.sh

---
+++ hrtg/hotspot-hotrap.sh
@@ -0,0 +1,14 @@
+set -e
+workload_file=../workloads/config_4e7_read_0.5_insert_0.5_hotspot_hotspotdatafraction_0.1_hotspotopnfraction_0.9
+# hit_count, delta=2GB, SD=4GB, kAccurateHotSizePromotionSize
+DIR=../../data/$(basename $0 .sh)
+./hotrap.sh 4GB 2GB $workload_file $DIR
+mkdir -p $DIR/plot/
+../plot/du.py $DIR &
+../plot/ops.py $DIR 10 &
+../plot/tps.py $DIR 10 &
+../plot/throughput.py $DIR 100 &
+../plot/hit.py $DIR &
+../plot/promoted-bytes.py $DIR &
+../plot/latency.py < $DIR/latency > $DIR/plot/latency &
+wait
---
+++ hrtg/hotspot-rocksdb.sh
@@ -0,0 +1,11 @@
+set -e
+workload_file=../workloads/config_4e7_read_0.5_insert_0.5_hotspot_hotspotdatafraction_0.1_hotspotopnfraction_0.9
+# SD=4GB
+DIR=../../data/$(basename $0 .sh)
+./rocksdb.sh 4GB $workload_file $DIR
+mkdir -p $DIR/plot/
+../plot/ops.py $DIR 10 &
+../plot/tps.py $DIR 10 &
+../plot/throughput.py $DIR 100 &
+../plot/latency.py < $DIR/latency > $DIR/plot/latency &
+wait
