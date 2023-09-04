4604e75748b4ca721614471d39a98a4d7641a90d
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-09-04 17:39:46 +0800

    Make latency.py noop

--- plot/latency.py
+++ plot/latency.py
@@ -1,13 +1,1 @@
 #!/usr/bin/env python3
-import sys
-import pandas as pd
-import numpy as np
-latencies = pd.read_table(sys.stdin, names=['operation', 'latency(ns)'], delim_whitespace=True)
-res = latencies.groupby(latencies['operation'])['latency(ns)'].apply(list)
-if 'INSERT' in res:
-    insert = np.array(res['INSERT'])
-    print('Insert(ns):')
-    print(np.percentile(insert, [10, 20, 30, 40, 50, 60, 70, 80, 90, 99, 99.9, 99.99]))
-read = np.array(res['READ'])
-print('Read(ns):')
-print(np.percentile(read, [10, 20, 30, 40, 50, 60, 70, 80, 90, 99, 99.9, 99.99]))
