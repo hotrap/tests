715a6723fe08042cd35c752da2dc3253a36bc19a
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-09-05 20:02:02 +0800

    Argument paths should be absolute

--- workloads/ycsb-hotrap.sh
+++ workloads/ycsb-hotrap.sh
@@ -14,7 +14,6 @@ if [ "$res" ]; then
 fi
 sd_size=$(humanfriendly --parse-size=$1)
 max_hot_set_size=$(humanfriendly --parse-size=$2)
-kvexe_dir=../../kvexe/build/
 workload_file=$(realpath "$3")
 DIR=$(realpath "$4")
 if [ $5 ]; then
@@ -22,15 +21,17 @@ if [ $5 ]; then
 else
 	num_threads=1
 fi
-# kAccurateHotSizePromotionSize
+workspace=$(realpath ../..)
+kvexe_dir=$workspace/kvexe/build/
+
 ulimit -n 100000
-(cd ../../YCSB && ./bin/ycsb load basic -P $workload_file) | $kvexe_dir/rocksdb-kvexe --cleanup --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --db_path=../../testdb/db/ --db_paths="{{../../testdb/sd,$sd_size},{../../testdb/cd,100000000000}}" --viscnts_path=../../testdb/viscnts 2>> $4/log.txt
+(cd ../../YCSB && ./bin/ycsb load basic -P $workload_file) | $kvexe_dir/rocksdb-kvexe --cleanup --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --db_path=$workspace/testdb/db/ --db_paths="{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,100000000000}}" --viscnts_path=$workspace/testdb/viscnts 2>> $4/log.txt
 cd ../../testdb/
 du -sh db/ sd/ cd/ >> $DIR/log.txt
 cd - > /dev/null
 
 tmp_dir=$(mktemp -d)
-../helper/exe-while.sh $tmp_dir bash -c "set -e; set -o pipefail; (cd ../../YCSB && ./bin/ycsb run basic -P $workload_file) | tee >(../helper/bin/trace-cleaner | awk '{if (\$1 == \"READ\") print \$3}' | ../helper/bin/occurrences > ../../testdb/db/occurrences) | $kvexe_dir/rocksdb-kvexe --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --switches=0xd --num_threads=$num_threads --db_path=../../testdb/db/ --db_paths=\"{{../../testdb/sd,$sd_size},{../../testdb/cd,100000000000}}\" --viscnts_path=../../testdb/viscnts 2>> $4/log.txt"
+../helper/exe-while.sh $tmp_dir bash -c "set -e; set -o pipefail; (cd ../../YCSB && ./bin/ycsb run basic -P $workload_file) | tee >(../helper/bin/trace-cleaner | awk '{if (\$1 == \"READ\") print \$3}' | ../helper/bin/occurrences > ../../testdb/db/occurrences) | $kvexe_dir/rocksdb-kvexe --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --switches=0xd --num_threads=$num_threads --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,100000000000}}\" --viscnts_path=$workspace/testdb/viscnts 2>> $4/log.txt"
 mv -n $tmp_dir/* $4/
 rm -r $tmp_dir
 bash ../helper/hotrap-data.sh "$DIR"
--- workloads/ycsb-rocksdb.sh
+++ workloads/ycsb-rocksdb.sh
@@ -13,7 +13,6 @@ if [ "$res" ]; then
         exit 1
 fi
 sd_size=$(humanfriendly --parse-size=$1)
-kvexe_dir=../../kvexe-rocksdb/build/
 workload_file=$(realpath $2)
 DIR=$(realpath "$3")
 if [ $4 ]; then
@@ -21,14 +20,17 @@ if [ $4 ]; then
 else
 	num_threads=1
 fi
+workspace=$(realpath ../..)
+kvexe_dir=$workspace/kvexe-rocksdb/build/
+
 ulimit -n 100000
-(cd ../../YCSB && ./bin/ycsb load basic -P $workload_file) | $kvexe_dir/rocksdb-kvexe --cleanup --db_path=../../testdb/db/ --db_paths="{{../../testdb/sd,$sd_size},{../../testdb/cd,100000000000}}" 2>> $3/log.txt
+(cd ../../YCSB && ./bin/ycsb load basic -P $workload_file) | $kvexe_dir/rocksdb-kvexe --cleanup --db_path=$workspace/testdb/db/ --db_paths="{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,100000000000}}" 2>> $3/log.txt
 cd ../../testdb/
 du -sh db/ sd/ cd/ >> $DIR/log.txt
 cd - > /dev/null
 
 tmp_dir=$(mktemp -d)
-../helper/exe-while.sh $tmp_dir bash -c "set -e; set -o pipefail; (cd ../../YCSB && ./bin/ycsb run basic -P $workload_file) | $kvexe_dir/rocksdb-kvexe --switches=0x1 --num_threads=$num_threads --db_path=../../testdb/db/ --db_paths=\"{{../../testdb/sd,$sd_size},{../../testdb/cd,100000000000}}\" 2>> $3/log.txt"
+../helper/exe-while.sh $tmp_dir bash -c "set -e; set -o pipefail; (cd ../../YCSB && ./bin/ycsb run basic -P $workload_file) | $kvexe_dir/rocksdb-kvexe --switches=0x1 --num_threads=$num_threads --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,100000000000}}\" 2>> $3/log.txt"
 mv -n $tmp_dir/* $3/
 rm -r $tmp_dir
 bash ../helper/rocksdb-data.sh "$DIR"
