5451250a8f9ecf3713930c80487e657ae0af072f
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-09-06 12:18:06 +0800

    Update uniform-ro-8-rocksdb.sh

--- workloads/uniform-ro-8-rocksdb.sh
+++ workloads/uniform-ro-8-rocksdb.sh
@@ -5,8 +5,8 @@ DIR=../../data/$(basename $0 .sh)
 ./ycsb-rocksdb.sh 4GB $workload_file $DIR 8
 mkdir -p $DIR/plot/
 ../plot/du.py $DIR &
-../plot/ops.py $DIR 10 &
-../plot/tps.py $DIR 10 &
+../plot/ops.py $DIR 1 &
+../plot/tps.py $DIR 1 &
 ../plot/throughput.py $DIR 10 &
 wait
 
