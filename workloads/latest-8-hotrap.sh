1f58619d3e9eea2f9df14c783020e924170025dd
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-09-09 14:07:39 +0800

    Update mean step

--- workloads/latest-8-hotrap.sh
+++ workloads/latest-8-hotrap.sh
@@ -5,7 +5,7 @@ DIR=../../data/$(basename $0 .sh)
 ./ycsb-hotrap.sh 4GB 2GB $workload_file $DIR 8
 mkdir -p $DIR/plot/
 ../plot/du.py $DIR &
-../plot/ops.py $DIR 10 &
+../plot/ops.py $DIR 1 &
 ../plot/tps.py $DIR 1 &
 ../plot/throughput.py $DIR 10 &
 ../plot/hit.py $DIR &
--- workloads/latest-8-rocksdb.sh
+++ workloads/latest-8-rocksdb.sh
@@ -5,7 +5,7 @@ DIR=../../data/$(basename $0 .sh)
 ./ycsb-rocksdb.sh 4GB $workload_file $DIR 8
 mkdir -p $DIR/plot/
 ../plot/du.py $DIR &
-../plot/ops.py $DIR 50 &
-../plot/tps.py $DIR 50 &
-../plot/throughput.py $DIR 100 &
+../plot/ops.py $DIR 1 &
+../plot/tps.py $DIR 1 &
+../plot/throughput.py $DIR 10 &
 wait
