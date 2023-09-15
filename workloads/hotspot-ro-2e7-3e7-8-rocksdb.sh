757c053b0dbfe951f2272b87e05d363722e47ca8
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-09-15 21:19:26 +0800

    Fix

--- workloads/hotspot-ro-2e7-3e7-8-rocksdb.sh
+++ workloads/hotspot-ro-2e7-3e7-8-rocksdb.sh
@@ -1,6 +1,5 @@
 set -e
 workload_file=../config/workload_2e7_3e7_read_1_hotspot_hotspotdatafraction_0.1_hotspotopnfraction_0.9
-SD=4GB
 DIR=../../data/$(basename $0 .sh)
 ./ycsb-rocksdb.sh 4GB $workload_file $DIR 8
 mkdir -p $DIR/plot/
