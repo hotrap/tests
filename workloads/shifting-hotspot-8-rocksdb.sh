1981bcc64e15dd727c19f823e98c7315fe2acc7e
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-09-15 19:35:54 +0800

    Fix

--- workloads/shifting-hotspot-8-rocksdb.sh
+++ workloads/shifting-hotspot-8-rocksdb.sh
@@ -4,7 +4,8 @@ workload_file=../config/config_read_0.5_insert_0.5_shifting_hotspot_hotspotdataf
 DIR=../../data/$(basename $0 .sh)
 ./hrtg-rocksdb.sh 4GB $workload_file $DIR 8
 mkdir -p $DIR/plot/
-../plot/ops.py $DIR 10 &
-../plot/tps.py $DIR 10 &
-../plot/throughput.py $DIR 100 &
+../plot/du.py $DIR &
+../plot/ops.py $DIR 1 &
+../plot/tps.py $DIR 1 &
+../plot/throughput.py $DIR 10 &
 wait
