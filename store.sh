d2a974cfdc54cc142920579c4725b763af585523
yfzcsc <yfzcsc@gmail.com>
2023-09-29 13:41:44 +0800

    test scripts by yfzcsc

---
+++ store.sh
@@ -0,0 +1,19 @@
+mkdir $DIR
+cp $workload_file $DIR/workload
+cp ~/testdb/db/{LOG,log.txt,cpu,first-level-in-cd,progress,promoted-*,rocksdb-stats.txt,period_stats,latency} $DIR/
+cp ~/testdb/log/* $DIR/
+
+
+
+mkdir -p $DIR/plot/
+~/tests/plot/du.py $DIR &
+~/tests/plot/ops.py $DIR 10 &
+~/tests/plot/tps.py $DIR 1 &
+~/tests/plot/throughput.py $DIR 10 &
+~/tests/plot/hit.py $DIR &
+~/tests/plot/promoted-bytes.py $DIR &
+~/tests/plot/latency.py < $DIR/latency > $DIR/plot/latency &
+wait
+cd $DIR/plot
+rm all.pdf
+pdfunite *.pdf all.pdf
