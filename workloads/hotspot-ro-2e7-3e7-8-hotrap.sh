7cd992c0be9bd40cdddf1f87097f0786c6ce629d
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-10-06 18:28:13 +0800

    Add max hot set size to SD size

--- workloads/hotspot-ro-2e7-3e7-8-hotrap.sh
+++ workloads/hotspot-ro-2e7-3e7-8-hotrap.sh
@@ -1,8 +1,7 @@
 set -e
 workload_file=../config/workload_2e7_3e7_read_1_hotspot_hotspotdatafraction_0.1_hotspotopnfraction_0.9
-# hit_count, delta=2GB, SD=4GB, kAccurateHotSizePromotionSize
 DIR=../../data/$(basename $0 .sh)
-./ycsb-hotrap.sh 4GB 2GB $workload_file $DIR 8
+./ycsb-hotrap.sh 6GB 2GB $workload_file $DIR 8
 mkdir -p $DIR/plot/
 ../plot/du.py $DIR &
 ../plot/ops.py $DIR 10 &
--- workloads/zipfian-8-hotrap.sh
+++ workloads/zipfian-8-hotrap.sh
@@ -1,8 +1,7 @@
 set -e
 workload_file=../config/workload_1e6_4e7_read_0.5_insert_0.5_zipfian
-# hit_count, delta=2GB, SD=4GB, kAccurateHotSizePromotionSize
 DIR=../../data/$(basename $0 .sh)
-./ycsb-hotrap.sh 4GB 2GB $workload_file $DIR 8
+./ycsb-hotrap.sh 6GB 2GB $workload_file $DIR 8
 mkdir -p $DIR/plot/
 ../plot/du.py $DIR &
 ../plot/ops.py $DIR 10 &
