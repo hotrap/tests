76826de3753fcd78fa08f76f10ad085e227d21cc
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-10-17 21:32:57 +0800

    hotrap-plot-200GB.sh and rocksdb-plot-200GB.sh

---
+++ helper/hotrap-plot-200GB.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if [ ! $1 ]; then
+	echo Usage: $0 dir
+	exit 1
+fi
+DIR=$(realpath "$1")
+cd $(dirname $0)
+mkdir -p $DIR/plot/
+../plot/du.py $DIR &
+../plot/ops.py $DIR 10 &
+../plot/tps.py $DIR 10 &
+../plot/throughput.py $DIR 50 &
+../plot/hit.py $DIR &
+../plot/promoted-bytes.py $DIR &
+../plot/hit-rate.py $DIR &
+wait
