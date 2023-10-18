e594b8e9b93a323853875746ea22211f7af7275d
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-10-18 16:33:54 +0800

    200GB.sh

---
+++ workloads/200GB.sh
@@ -0,0 +1,30 @@
+workloads=(
+	"latest_200GB_read_0.5_insert_0.5"
+	"hotspot0.1_200GB_read_0.5_insert_0.5"
+	"uniform_200GB_read_0.5_insert_0.5"
+	"zipfian_200GB_read_0.5_insert_0.5"
+	"ycsbc_hotspot0.1_200GB"
+	"ycsbc_hotspotshifting0.1_200GB"
+	"ycsbc_uniform_200GB"
+	"ycsbc_zipfian_200GB"
+)
+function run_rocksdb {
+	../helper/checkout-$2
+	DIR=../../data/$1/$2
+	echo Result directory: $DIR
+	./test-rocksdb-200GB.sh ../config/$1 $DIR
+	../helper/rocksdb-plot-200GB.sh $DIR
+}
+function run_hotrap {
+	../helper/checkout-$2
+	DIR=../../data/$1/$2
+	echo Result directory: $DIR
+	./test-hotrap-200GB.sh 20GB ../config/$1 $DIR
+	../helper/hotrap-plot-200GB.sh $DIR
+}
+for workload in "${workloads[@]}"; do
+	run_rocksdb $workload rocksdb
+	run_rocksdb $workload rocksdb-fat
+	run_hotrap $workload flush-accessed
+	run_hotrap $workload flush-stably-hot
+done
---
+++ workloads/test-hotrap-200GB.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+if [[ $# < 3 || $# > 4 ]]; then
+	echo Usage: $0 max-hot-size workload-file output-dir [switches]
+	exit 1
+fi
+set -e
+set -o pipefail
+# To make set -e take effect if the output dir does not exists
+mkdir -p $3
+res="$(ls -A $3)"
+if [ "$res" ]; then
+	echo "$3" is not empty!
+	exit 1
+fi
+max_hot_set_size=$(humanfriendly --parse-size=$1)
+workload_file=$(realpath "$2")
+DIR=$(realpath "$3")
+if [ $4 ]; then
+	switches=$4
+else
+	switches=0x9
+fi
+cd "$(dirname $0)"
+workspace=$(realpath ../..)
+kvexe_dir=$workspace/kvexe/build/
+
+ulimit -n 100000
+../helper/exe-while.sh $DIR bash -c "$kvexe_dir/rocksdb-kvexe --cleanup --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --switches=$switches --num_threads=8 --max_background_jobs=4 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --export_key_only_trace --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,60000000000},{$workspace/testdb/cd,100000000000}}\" --viscnts_path=$workspace/testdb/viscnts 2>> $DIR/log.txt"
+bash ../helper/hotrap-data.sh "$DIR"
