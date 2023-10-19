7ff6c0f9ed8a139701be650f5738ecd34e3d4c1b
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-10-19 22:10:20 +0800

    --max_background_jobs=6

--- workloads/test-hotrap-200GB.sh
+++ workloads/test-hotrap-200GB.sh
@@ -25,5 +25,5 @@ workspace=$(realpath ../..)
 kvexe_dir=$workspace/kvexe/build/
 
 ulimit -n 100000
-../helper/exe-while.sh $DIR bash -c "$kvexe_dir/rocksdb-kvexe --cleanup --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --switches=$switches --num_threads=8 --max_background_jobs=4 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --export_key_only_trace --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,60000000000},{$workspace/testdb/cd,100000000000}}\" --viscnts_path=$workspace/testdb/viscnts 2>> $DIR/log.txt"
+../helper/exe-while.sh $DIR bash -c "$kvexe_dir/rocksdb-kvexe --cleanup --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --switches=$switches --num_threads=8 --max_background_jobs=6 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --export_key_only_trace --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,60000000000},{$workspace/testdb/cd,100000000000}}\" --viscnts_path=$workspace/testdb/viscnts 2>> $DIR/log.txt"
 bash ../helper/hotrap-data.sh "$DIR"
