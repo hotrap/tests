ed52bb3d7ea1e3f270043820da234e0d866a966b
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-10-25 06:15:45 +0000

    Invoke rocksdb-plot-200GB.sh in hotrap-plot-200GB.sh

--- helper/hotrap-plot-200GB.sh
+++ helper/hotrap-plot-200GB.sh
@@ -6,10 +6,7 @@ fi
 DIR=$(realpath "$1")
 cd $(dirname $0)
 mkdir -p $DIR/plot/
-../plot/du.py $DIR &
-../plot/ops.py $DIR 10 &
-../plot/tps.py $DIR 10 &
-../plot/throughput.py $DIR 50 &
+bash rocksdb-plot-200GB.sh $DIR &
 ../plot/hit.py $DIR &
 ../plot/promoted-bytes.py $DIR &
 ../plot/hit-rate.py $DIR &
