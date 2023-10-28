a8b0e8cf6ccbd309fb70ec773db748c3be0c5fe0
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-10-28 17:16:01 +0800

    Disk usage of VisCnts

--- helper/jobs-periodic/du.sh
+++ helper/jobs-periodic/du.sh
@@ -2,10 +2,10 @@
 
 function work {
 	cd ../../../testdb/
-	echo Timestamp\(ns\) DB SD CD
+	echo Timestamp\(ns\) DB SD CD VisCnts
 	while true; do
 		echo -n "$(date +%s%N) "
-		du -sb db/ sd/ cd/ | awk '{print $1}' | xargs echo
+		du -sb db/ sd/ cd/ viscnts/ | awk '{print $1}' | xargs echo
 		sleep 1
 	done
 }
