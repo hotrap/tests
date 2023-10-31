7f0863c3c4ab893485818242ce607ba82f0a88fe
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-01 00:24:28 +0800

    save info.json

--- helper/rocksdb-data.sh
+++ helper/rocksdb-data.sh
@@ -10,7 +10,7 @@ cd $mydir
 cd ../../testdb
 du -sh db/ sd/ cd/ >> "$DIR"/log.txt
 cd db
-mv LOG rocksdb-stats*.txt progress cpu mem "$DIR"/
+mv LOG rocksdb-stats*.txt progress cpu mem info.json "$DIR"/
 if [ -f key_only_trace_0 ]; then
 	find . -name "key_only_trace_*" -exec cat {} \; | awk '{if ($1 == "READ") print $2}' | $mydir/bin/occurrences > occurrences
 fi
