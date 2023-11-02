302ec552fd34f7e07d9451cae8d3af8b575ffc94
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-03 00:30:35 +0800

    11GB.sh: max hot set size should be 110MB

--- workloads/11GB.sh
+++ workloads/11GB.sh
@@ -21,7 +21,7 @@ function run_hotrap {
 	../helper/checkout-$2
 	DIR=../../data/$1/$2
 	echo Result directory: $DIR
-	./test-hotrap-11GB.sh ../config/$1 $DIR 1GB 100MB
+	./test-hotrap-11GB.sh ../config/$1 $DIR 1GB 110MB
 	../helper/hotrap-plot.sh $DIR
 }
 for workload in "${workloads[@]}"; do
