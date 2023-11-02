35a47c96916d7b2562e4086f65e0490d690bee41
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-02 19:21:53 +0800

    11GB workloads

---
+++ workloads/11GB.sh
@@ -0,0 +1,31 @@
+workloads=(
+	"ycsbc_hotspot0.01_11GB"
+	"ycsbc_hotspotshifting0.01_11GB"
+	"ycsbc_uniform_11GB"
+	"ycsbc_zipfian_11GB"
+	"hotspot0.01_11GB_read_0.5_insert_0.5"
+	"latest_11GB_read_0.5_insert_0.5"
+	"uniform_11GB_read_0.5_insert_0.5"
+	"zipfian_11GB_read_0.5_insert_0.5"
+	"ycsbc_hotspot0.01_11GB_200B"
+	"hotspot0.01_11GB_200B_read_0.5_insert_0.5"
+)
+function run_rocksdb {
+	../helper/checkout-$2
+	DIR=../../data/$1/$2
+	echo Result directory: $DIR
+	./test-rocksdb-11GB.sh ../config/$1 $DIR 1GB
+	../helper/rocksdb-plot.sh $DIR
+}
+function run_hotrap {
+	../helper/checkout-$2
+	DIR=../../data/$1/$2
+	echo Result directory: $DIR
+	./test-hotrap-11GB.sh ../config/$1 $DIR 1GB 100MB
+	../helper/hotrap-plot.sh $DIR
+}
+for workload in "${workloads[@]}"; do
+	run_rocksdb $workload rocksdb
+	run_rocksdb $workload rocksdb-fat
+	run_hotrap $workload flush-stably-hot
+done
---
+++ workloads/test-rocksdb-11GB.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+if [[ $# < 3 || $# > 5 ]]; then
+	echo Usage: $0 workload-file output-dir sd-size [switches]
+	exit 1
+fi
+set -e
+set -o pipefail
+# To make set -e take effect if the output dir does not exists
+mkdir -p $2
+res="$(ls -A $2)"
+if [ "$res" ]; then
+	echo "$2" is not empty!
+	exit 1
+fi
+workload_file=$(realpath $1)
+DIR=$(realpath "$2")
+sd_size=$(humanfriendly --parse-size=$3)
+if [ $4 ]; then
+	switches=$4
+else
+	switches=0x1
+fi
+cd "$(dirname $0)"
+workspace=$(realpath ../..)
+kvexe_dir=$workspace/kvexe-rocksdb/build/
+
+ulimit -n 100000
+../helper/exe-while.sh $DIR bash -c "$kvexe_dir/rocksdb-kvexe --cleanup --switches=$switches --num_threads=8 --max_background_jobs=4 --block_size=16384 --max_bytes_for_level_base=67108864 --level0_file_num_compaction_trigger=1 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,100000000000}}\" 2>> $DIR/log.txt"
+bash ../helper/rocksdb-data.sh "$DIR"
