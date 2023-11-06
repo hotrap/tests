18938400cf4f59dc78d0147c2fdf2f4bc64800d0
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-06 23:12:31 +0800

    11GB-no-retain.sh

---
+++ workloads/11GB-no-retain.sh
@@ -0,0 +1,13 @@
+workloads=(
+	"hotspot0.01_11GB_read_0.5_insert_0.5"
+)
+function run_hotrap {
+	../helper/checkout-$2
+	DIR=../../data/$1/$2
+	echo Result directory: $DIR
+	./test-hotrap-11GB.sh ../config/$1 $DIR 1GB 110MB
+	../helper/hotrap-plot-11GB.sh $DIR
+}
+for workload in "${workloads[@]}"; do
+	run_hotrap $workload no-retain
+done
