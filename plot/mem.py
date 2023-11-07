21e2c2b1e6343c89cc8faa891f824c94482566c9
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-07 20:41:31 +0800

    plot/mem.py

---
+++ plot/mem.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) != 2:
+	print('Usage: ' + sys.argv[0] + ' dir')
+	exit()
+
+import os
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+fontsize=9
+fonten = {'family': 'Times New Roman', 'size': fontsize}
+
+mpl.rcParams.update({
+    'font.family': 'sans-serif',
+    'font.sans-serif': ['Times New Roman'],
+    })  # 设置全局字体
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+
+d = sys.argv[1]
+mem = pd.read_table(d + '/mem', delim_whitespace=True)
+time = (mem['Timestamp(ns)'] - mem['Timestamp(ns)'][0]) / 1e9
+mem = mem['RSS(KB)'] / 1e6
+
+plot_dir = d + '/plot'
+if not os.path.exists(plot_dir):
+	os.system('mkdir -p ' + plot_dir)
+pdf_path = plot_dir + '/mem.pdf'
+plt.plot(time, mem)
+plt.xlabel('Time (Seconds)', fontdict=fonten)
+plt.ylabel('Memory usage (GB)', fontdict=fonten)
+plt.savefig(pdf_path)
+print('Plot saved to ' + pdf_path)
+if 'DISPLAY' in os.environ:
+	plt.show()
