704daf4cd02527ee773056501edb1093e9315bb4
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-14 13:32:34 +0800

    workloads/110GB-no-promote-by-compaction.sh

---
+++ workloads/110GB-no-promote-by-compaction.sh
@@ -0,0 +1,14 @@
+workloads=(
+	"read_0.5_insert_0.5_hotspot0.01_110GB"
+)
+function run_hotrap {
+	../helper/checkout-$2
+	DIR=../../data/$1/$2
+	echo Result directory: $DIR
+	# Reserve 250MB for VisCnts
+	./test-hotrap-110GB.sh ../config/$1 $DIR 9.75GB 1.1GB
+	../helper/hotrap-plot.sh $DIR
+}
+for workload in "${workloads[@]}"; do
+	run_hotrap $workload no-promote-by-compaction
+done
