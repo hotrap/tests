a72703a9069770ff91765d14dab25982c9451fd0
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-19 20:05:22 +0800

    Add RocksDB(SD) into ycsb-sweep.py

--- plot/ycsb-sweep.py
+++ plot/ycsb-sweep.py
@@ -31,16 +31,30 @@ mpl.rcParams.update({
 })
 plt.rcParams['axes.unicode_minus'] = False
 
-fig = plt.figure(dpi = 300, figsize = (cm_to_inch(DOUBLE_COL_WIDTH), cm_to_inch(5)))
+fig = plt.figure(dpi = 300, figsize = (cm_to_inch(DOUBLE_COL_WIDTH), cm_to_inch(4)))
 
-subfig_titles = ['(a) hotspot 1%', '(b) zipfian', '(c) uniform']
+subfigs = [
+    {
+        'title': '(a) hotspot 1%',
+        'ticks': [0, 1e4, 2e4, 3e4, 4e4],
+    },
+    {
+        'title': '(b) zipfian',
+        'ticks': [0, 2e4, 4e4, 6e4],
+    },
+    {
+        'title': '(c) uniform',
+        'ticks': [0, 1e4, 2e4, 3e4, 4e4],
+    }
+]
 colormap = 'Set2'
-patterns = ['///', '\\\\\\', '', 'XXX', '---']
 
 workloads=['hotspot0.01', 'zipfian', 'uniform']
 ycsb_configs=['ycsba', 'read_0.5_insert_0.5', 'ycsbc', 'ycsbd', 'ycsbf']
 cluster_labels = ['A', 'A\'', 'C', 'D', 'F']
-versions=['flush-stably-hot', 'rocksdb-fat']
+versions=['flush-stably-hot', 'rocksdb-fat', 'rocksdb-sd']
+patterns = ['///', '\\\\\\', 'XXXXXXXXX', 'XXX', '---']
+version_names = ['HotRAP', 'RocksDB-fat', 'RocksDB(SD)']
 size='110GB'
 
 gs = gridspec.GridSpec(1, len(workloads))
@@ -53,7 +67,6 @@ for i in range(len(workloads)):
     ax.set_axisbelow(True)
     ax.grid(axis='y')
     workload = workloads[i]
-    max_value = 0
     for (pivot, ycsb) in enumerate(ycsb_configs):
         workload_dir = os.path.join(dir, ycsb + '_' + workload + '_' + size)
         for (version_idx, version) in enumerate(versions):
@@ -67,20 +80,17 @@ for i in range(len(workloads)):
             operations_executed = progress.iloc[-1]['operations-executed'] - progress.iloc[0]['operations-executed']
             seconds = (progress.iloc[-1]['Timestamp(ns)'] - progress.iloc[0]['Timestamp(ns)']) / 1e9
             value = operations_executed / seconds
-            if value > max_value:
-                max_value = value
             ax.bar(x, value, width=bar_width, hatch=patterns[version_idx], color=plt.get_cmap(colormap)(version_idx), edgecolor='black', linewidth=0.5)
     formatter = ScalarFormatter(useMathText=True)
     formatter.set_powerlimits((-3, 4))
     ax.yaxis.set_major_formatter(formatter)
     ax.yaxis.get_offset_text().set_fontsize(8)
     plt.xticks(range(0, len(cluster_labels)), cluster_labels, fontsize=8)
-    plt.yticks(fontsize=8)
-    plt.ylim(0, math.ceil(max_value / 1e4) * 1e4)
-    plt.xlabel(subfig_titles[i], labelpad=1, fontsize=8)
+    plt.yticks(subfigs[i]['ticks'], fontsize=8)
+    plt.xlabel(subfigs[i]['title'], labelpad=1, fontsize=8)
     if i == 0:
         plt.ylabel('Operations per second', fontsize=8)
-fig.legend(versions, fontsize=8, ncol=len(versions), loc='center', bbox_to_anchor=(0.5, 0.99))
+fig.legend(version_names, fontsize=8, ncol=len(versions), loc='center', bbox_to_anchor=(0.5, 0.99))
 plt.tight_layout()
 pdf_path = dir + '/ycsb-sweep.pdf'
 plt.savefig(pdf_path, bbox_inches='tight', pad_inches=0.01)
