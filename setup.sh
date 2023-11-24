424a164391c53fa93845924337b6c3c330b2edcf
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-24 20:53:00 +0800

    run-secondary-cache

--- setup.sh
+++ setup.sh
@@ -17,6 +17,11 @@ cd ..
 cd ..
 mkdir data
 
+git clone -b v2023.11.20.00 https://github.com/facebook/CacheLib.git
+cd CacheLib
+./contrib/build.sh -j -T
+echo "export CACHELIB_HOME=$HOME/CacheLib/opt/cachelib" >> ~/.profile
+
 git clone git@github.com:hotrap/hotrap.git rocksdb
 cp -r rocksdb hotrap
 
@@ -26,6 +31,7 @@ cd kvexe-rocksdb
 rm -r 3rdparty/{rcu-vector,counter-timer-vec}
 git checkout rocksdb
 cd ..
+git clone -b secondary-cache --recursive git@github.com:hotrap/kvexe.git kvexe-secondary-cache
 
 git clone git@github.com:hotrap/viscnts-splay-rs.git
 git clone --recursive git@github.com:hotrap/viscnts-lsm.git
@@ -34,4 +40,4 @@ echo Need to setup manually:
 echo 1. testdb/{db,sd,cd,viscnts}
 echo 2. export sd_dev=the device in \"iostat\" that is used as SD.
 echo 3. export cd_dev=the device in \"iostat\" that is used as CD.
-echo 4. It is recommended to restart your shell to make changes take effect
+echo 4. Restart your shell to make changes take effect
--- workloads/110GB-hotset0.05.sh
+++ workloads/110GB-hotset0.05.sh
@@ -16,6 +16,20 @@ workloads=(
 	"ycsbf_zipfian_110GB"
 	"ycsbc_hotspotshifting0.01_110GB"
 )
+function run-rocksdb-sd {
+	../helper/checkout-rocksdb
+	DIR=../../data/$1/rocksdb-sd
+	echo Result directory: $DIR
+	./test-rocksdb-sd-110GB.sh ../config/$1 $DIR
+	../helper/rocksdb-plot.sh $DIR
+}
+function run-secondary-cache {
+	../helper/checkout-secondary-cache
+	DIR=../../data/$1/secondary-cache
+	echo Result directory: $DIR
+	./test-secondary-cache-110GB.sh ../config/$1 $DIR 10GB 5.5GB
+	../helper/rocksdb-plot.sh $DIR
+}
 function run-rocksdb {
 	../helper/checkout-$2
 	DIR=../../data/$1/$2
@@ -31,17 +45,11 @@ function run-hotrap {
 	./test-hotrap-110GB.sh ../config/$1 $DIR 9.75GB 5.5GB
 	../helper/hotrap-plot.sh $DIR
 }
-function run-rocksdb-sd {
-	../helper/checkout-rocksdb
-	DIR=../../data/$1/rocksdb-sd
-	echo Result directory: $DIR
-	./test-rocksdb-sd-110GB.sh ../config/$1 $DIR
-	../helper/rocksdb-plot.sh $DIR
-}
 for workload in "${workloads[@]}"; do
+	run-rocksdb-sd $workload
+	run-secondary-cache $workload
+	run-rocksdb $workload rocksdb-fat
 	run-hotrap $workload with-probation
 	run-hotrap $workload viscnts-splay-rs
 	run-hotrap $workload flush-stably-hot
-	run-rocksdb $workload rocksdb-fat
-	run-rocksdb-sd $workload
 done
