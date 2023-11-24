bfda84a4b44bb8108d22b7f3004857216a18cf46
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-24 17:18:26 +0800

    11GB hotset0.05 by default

--- workloads/11GB-no-promote-by-compaction.sh
+++ workloads/11GB-no-promote-by-compaction.sh
@@ -1,12 +1,12 @@
 workloads=(
-	"read_0.5_insert_0.5_hotspot0.01_11GB"
+	"read_0.5_insert_0.5_hotspot0.05_11GB"
 )
 function run_hotrap {
 	../helper/checkout-$2
 	DIR=../../data/$1/$2
 	echo Result directory: $DIR
 	# Reserve 25MB for VisCnts
-	./test-hotrap-11GB.sh ../config/$1 $DIR 0.975GB 110MB
+	./test-hotrap-11GB.sh ../config/$1 $DIR 0.975GB 550MB
 	../helper/hotrap-plot-11GB.sh $DIR
 }
 for workload in "${workloads[@]}"; do
