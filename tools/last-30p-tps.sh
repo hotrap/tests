fbb391209556a15e7f3fd90fc9c25824aee22ef7
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-28 15:58:00 +0800

    tools/last-30p-tps.sh

---
+++ tools/last-30p-tps.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+timestamp_70p=$(hjson-cli -j < info.json | jq -r ".\"run-70%-timestamp(ns)\"")
+timestamp_100p=$(hjson-cli -j < info.json | jq -r ".\"run-end-timestamp(ns)\"")
+function throughput {
+	awk "
+		BEGIN {
+			r = 0
+			w = 0
+		}
+		{
+			if (NR > 1 && $timestamp_70p <= \$1 && \$1 < $timestamp_100p) {
+				r += \$2
+				w += \$4
+			}
+		}
+		END {
+			seconds = ($timestamp_100p - $timestamp_70p) / 1e9
+			print \"read\", r / seconds
+			print \"write\", w / seconds
+		}
+	" < $1
+}
+echo SD
+throughput iostat-sd.txt
+echo CD
+throughput iostat-cd.txt
