084c4294d8cb959d5345489efc62dc4f14e05237
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-11-29 16:13:20 +0800

    max-viscnts-size

--- workloads/test-hotrap-110GB.sh
+++ workloads/test-hotrap-110GB.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ $# < 4 || $# > 5 ]]; then
-	echo Usage: $0 workload-file output-dir sd-size max-hot-size [switches]
+	echo Usage: $0 workload-file output-dir sd-size max-hot-size max-viscnts-size [switches]
 	exit 1
 fi
 set -e
@@ -16,8 +16,9 @@ workload_file=$(realpath -s "$1")
 DIR=$(realpath "$2")
 sd_size=$(humanfriendly --parse-size=$3)
 max_hot_set_size=$(humanfriendly --parse-size=$4)
-if [ $5 ]; then
-	switches=$5
+max_viscnts_size=$(humanfriendly --parse-size=$5)
+if [ $6 ]; then
+	switches=$6
 else
 	switches=0x9
 fi
@@ -26,5 +27,5 @@ workspace=$(realpath ../..)
 kvexe_dir=$workspace/kvexe/build/
 
 ulimit -n 100000
-../helper/exe-while.sh $DIR bash -c "$kvexe_dir/rocksdb-kvexe --cleanup --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --switches=$switches --num_threads=8 --max_background_jobs=4 --block_size=16384 --max_bytes_for_level_base=67108864 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --export_key_only_trace --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,100000000000}}\" --viscnts_path=$workspace/testdb/viscnts 2>> $DIR/log.txt"
+../helper/exe-while.sh $DIR bash -c "$kvexe_dir/rocksdb-kvexe --cleanup --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --max_viscnts_size=$max_viscnts_size --switches=$switches --num_threads=8 --max_background_jobs=4 --block_size=16384 --max_bytes_for_level_base=67108864 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --export_key_only_trace --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,100000000000}}\" --viscnts_path=$workspace/testdb/viscnts 2>> $DIR/log.txt"
 bash ../helper/hotrap-data.sh "$DIR"
