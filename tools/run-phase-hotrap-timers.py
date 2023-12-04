eaf81fbfc3c60e7135aa81725ad3f5145ed6cb2d
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-12-04 22:50:17 +0800

    tools/run-phase-hotrap-timers.py

--- tools/run-phase-hotrap-timers.py
+++ tools/run-phase-hotrap-timers.py
@@ -17,13 +17,13 @@ def get_timers(fname):
     return json5.loads(json_str)
 
 run = get_timers('rocksdb-stats-run.txt')
-run_70p = get_timers('rocksdb-stats-run-70p.txt')
+run_0p = get_timers('rocksdb-stats-load-finish.txt')
 res = []
 for i in range(0, len(run)):
     timer = run[i]
-    timer_70p = run_70p[i]
-    assert timer_70p['name'] == timer['name']
-    timer['count'] -= timer_70p['count']
-    timer['time(sec)'] -= timer_70p['time(sec)']
+    timer_0p = run_0p[i]
+    assert timer_0p['name'] == timer['name']
+    timer['count'] -= timer_0p['count']
+    timer['time(sec)'] -= timer_0p['time(sec)']
     res.append(timer)
 print(json5.dumps(res, indent=4, trailing_commas=False))
