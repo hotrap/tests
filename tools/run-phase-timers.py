b037c4e733ba0de7bb31c4597a052f9473879e71
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-12-06 11:56:58 +0800

    Update tools/run-phase-timers.py

--- tools/run-phase-timers.py
+++ tools/run-phase-timers.py
@@ -8,5 +8,7 @@ run_start_timestamp = info['run-start-timestamp(ns)']
 run_end_timestamp = info['run-end-timestamp(ns)']
 timers = pd.read_table('timers', delim_whitespace=True)
 timers = timers[(run_start_timestamp <= timers['Timestamp(ns)']) & (timers['Timestamp(ns)'] < run_end_timestamp)]
-compaction_cpu_time_secs = (timers.iloc[-1] - timers.iloc[0])['compaction-cpu-micros'] / 1e6
-print('Compaction CPU time(s): %f' %compaction_cpu_time_secs)
+
+print('Compaction CPU time(s): %f' %((timers.iloc[-1] - timers.iloc[0])['compaction-cpu-micros'] / 1e6))
+print('Insert CPU time(s): %f' %((timers.iloc[-1] - timers.iloc[0])['insert-cpu-nanos'] / 1e9))
+print('Read CPU time(s): %f' %((timers.iloc[-1] - timers.iloc[0])['read-cpu-nanos'] / 1e9))
