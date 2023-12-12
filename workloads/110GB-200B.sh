443578cba916ce72c16e1b986989f1a71769e5a7
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-12-12 16:17:39 +0800

    Update workloads/110GB-200B.sh

--- workloads/110GB-200B.sh
+++ workloads/110GB-200B.sh
@@ -1,15 +1,20 @@
-workloads=(
+hotspot_workloads=(
 	"read_0.5_insert_0.5_hotspot0.05_110GB_200B"
 	"ycsba_hotspot0.05_110GB_200B"
 	"ycsbc_hotspot0.05_110GB_200B"
-	"ycsbd_hotspot0.05_110GB_200B"
-	"ycsbf_hotspot0.05_110GB_200B"
+)
+uniform_workloads=(
 	"read_0.5_insert_0.5_uniform_110GB_200B"
 	"ycsba_uniform_110GB_200B"
 	"ycsbc_uniform_110GB_200B"
-	"ycsbd_uniform_110GB_200B"
-	"ycsbf_uniform_110GB_200B"
 )
+function run-rocksdb-sd {
+	../helper/checkout-rocksdb
+	DIR=../../data/$1/rocksdb-sd
+	echo Result directory: $DIR
+	./test-rocksdb-sd-110GB-200B.sh ../config/$1 $DIR
+	../helper/rocksdb-plot.sh $DIR
+}
 function run-rocksdb {
 	../helper/checkout-$2
 	DIR=../../data/$1/$2
@@ -21,19 +26,15 @@ function run-hotrap {
 	../helper/checkout-$2
 	DIR=../../data/$1/$2
 	echo Result directory: $DIR
-	# Reserve 1.5GB for viscnts-lsm
-	./test-hotrap-110GB-200B.sh ../config/$1 $DIR 8.5GB 5.5GB
+	# Reserve 1.65GB for VisCnts
+	./test-hotrap-110GB-200B.sh ../config/$1 $DIR 8.35GB 5.5GB 1.65GB
 	../helper/hotrap-plot.sh $DIR
 }
-function run-rocksdb-sd {
-	../helper/checkout-rocksdb
-	DIR=../../data/$1/rocksdb-sd
-	echo Result directory: $DIR
-	./test-rocksdb-sd-110GB-200B.sh ../config/$1 $DIR
-	../helper/rocksdb-plot.sh $DIR
-}
-for workload in "${workloads[@]}"; do
+for workload in "${hotspot_workloads[@]}"; do
+	run-hotrap $workload flush-stably-hot
+	run-rocksdb-sd $workload
+done
+for workload in "${uniform_workloads[@]}"; do
 	run-hotrap $workload flush-stably-hot
 	run-rocksdb $workload rocksdb-fat
-	run-rocksdb-sd $workload
 done
