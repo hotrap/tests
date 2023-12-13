a13ee5d2a784dea1e80d535a66e953327fb21e53
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-12-13 22:17:04 +0800

    workloads/110GB-200B.sh: read_0.75_insert_0.25

--- workloads/110GB-200B.sh
+++ workloads/110GB-200B.sh
@@ -1,10 +1,12 @@
 hotspot_workloads=(
 	"read_0.5_insert_0.5_hotspot0.05_110GB_200B"
+	"read_0.75_insert_0.25_hotspot0.05_110GB_200B"
 	"ycsba_hotspot0.05_110GB_200B"
 	"ycsbc_hotspot0.05_110GB_200B"
 )
 uniform_workloads=(
 	"read_0.5_insert_0.5_uniform_110GB_200B"
+	"read_0.75_insert_0.25_uniform_110GB_200B"
 	"ycsba_uniform_110GB_200B"
 	"ycsbc_uniform_110GB_200B"
 )
