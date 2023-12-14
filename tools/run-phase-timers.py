a614b21ff32312ca17c249638f1e61873655a307
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-12-14 23:08:23 +0800

    run phase viscnts cpu time

--- tools/run-phase-timers.py
+++ tools/run-phase-timers.py
@@ -8,7 +8,10 @@ run_start_timestamp = info['run-start-timestamp(ns)']
 run_end_timestamp = info['run-end-timestamp(ns)']
 timers = pd.read_table('timers', delim_whitespace=True)
 timers = timers[(run_start_timestamp <= timers['Timestamp(ns)']) & (timers['Timestamp(ns)'] < run_end_timestamp)]
+timers = timers.iloc[-1] - timers.iloc[0]
 
-print('Compaction CPU time(s): %f' %((timers.iloc[-1] - timers.iloc[0])['compaction-cpu-micros'] / 1e6))
-print('Put CPU time(s): %f' %((timers.iloc[-1] - timers.iloc[0])['put-cpu-nanos'] / 1e9))
-print('Get CPU time(s): %f' %((timers.iloc[-1] - timers.iloc[0])['get-cpu-nanos'] / 1e9))
+print('Compaction CPU time(s): %f' %(timers['compaction-cpu-micros'] / 1e6))
+print('Put CPU time(s): %f' %(timers['put-cpu-nanos'] / 1e9))
+print('Get CPU time(s): %f' %(timers['get-cpu-nanos'] / 1e9))
+if 'viscnts.compaction.cpu.nanos' in timers:
+	print('VisCnts CPU time(s): %f' %((timers['viscnts.compaction.cpu.nanos'] + timers['viscnts.flush.cpu.nanos'] + timers['viscnts.decay.scan.cpu.nanos'] + timers['viscnts.decay.write.cpu.nanos']) / 1e9))
