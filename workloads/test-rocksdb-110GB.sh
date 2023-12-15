31481c0dbc135178230d943beef2308fa613bd0f
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-12-15 18:39:49 +0800

    Limit memory usage with systemd-run

--- workloads/test-rocksdb-110GB.sh
+++ workloads/test-rocksdb-110GB.sh
@@ -15,6 +15,7 @@ fi
 workload_file=$(realpath $1)
 DIR=$(realpath "$2")
 sd_size=$(humanfriendly --parse-size=$3)
+max_memory=$(humanfriendly --parse-size=1.1GB)
 if [ $4 ]; then
 	switches=$4
 else
@@ -25,5 +26,5 @@ workspace=$(realpath ../..)
 kvexe_dir=$workspace/kvexe-rocksdb/build/
 
 ulimit -n 100000
-../helper/exe-while.sh $DIR bash -c "LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 $kvexe_dir/rocksdb-kvexe --cleanup --switches=$switches --num_threads=8 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=67108864 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,1000000000000}}\" 2>> $DIR/log.txt"
+../helper/exe-while.sh $DIR bash -c "systemd-run --user -E LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 --scope -p MemoryMax=$max_memory $kvexe_dir/rocksdb-kvexe --cleanup --switches=$switches --num_threads=8 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=67108864 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,1000000000000}}\" 2>> $DIR/log.txt"
 bash ../helper/rocksdb-data.sh "$DIR"
--- workloads/test-rocksdb-sd-110GB-200B.sh
+++ workloads/test-rocksdb-sd-110GB-200B.sh
@@ -14,6 +14,7 @@ if [ "$res" ]; then
 fi
 workload_file=$(realpath $1)
 DIR=$(realpath "$2")
+max_memory=$(humanfriendly --parse-size=1.1GB)
 if [ $3 ]; then
 	switches=$3
 else
@@ -24,5 +25,5 @@ workspace=$(realpath ../..)
 kvexe_dir=$workspace/kvexe-rocksdb/build/
 
 ulimit -n 100000
-../helper/exe-while.sh $DIR bash -c "LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 $kvexe_dir/rocksdb-kvexe --cleanup --switches=$switches --num_threads=8 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=67108864 --optimize_filters_for_hits --enable_fast_generator --enable_fast_process --workload_file=$workload_file --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,1000000000000}}\" 2>> $DIR/log.txt"
+../helper/exe-while.sh $DIR bash -c "systemd-run --user -E LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 --scope -p MemoryMax=$max_memory $kvexe_dir/rocksdb-kvexe --cleanup --switches=$switches --num_threads=8 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=67108864 --optimize_filters_for_hits --enable_fast_generator --enable_fast_process --workload_file=$workload_file --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,1000000000000}}\" 2>> $DIR/log.txt"
 bash ../helper/rocksdb-data.sh "$DIR"
