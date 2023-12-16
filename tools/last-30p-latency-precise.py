5f5e1f5f42c3a7a967a225a29dca9e3ad228dc3e
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-12-16 18:42:14 +0800

    tools/stable-phase-latency.py

---
+++ tools/last-30p-latency-precise.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import json5
+import numpy as np
+import pandas as pd
+
+info = json5.load(open('info.json'))
+run_70p_ts = info['run-70%-timestamp(ns)']
+
+read_latencies = []
+insert_latencies = []
+i = 0
+while True:
+    path = 'latency-' + str(i)
+    if not os.path.exists(path):
+        break
+    latency = pd.read_table(path, delim_whitespace=True, names=['Timestamp(ns)', 'type', 'latency'])
+    latency = latency[latency['Timestamp(ns)'] >= run_70p_ts]
+    latency = latency.groupby('type')['latency'].apply(list)
+    if 'READ' in latency:
+        read_latencies.append(latency['READ'])
+    if 'INSERT' in latency:
+        insert_latencies.append(latency['INSERT'])
+    i += 1
+def work(latencies):
+    latencies = np.array([x for array in latencies for x in array])
+    print('Average %f ns' %(latencies.mean()))
+    percentiles = [99, 99.9]
+    a = np.percentile(latencies, percentiles)
+    for i in range(0, len(a)):
+        print('%f%% %f ns' %(percentiles[i], a[i]))
+print('Read')
+work(read_latencies)
+print('Insert')
+work(insert_latencies)
