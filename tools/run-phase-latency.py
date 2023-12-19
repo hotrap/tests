28649d3ba828df39433abd06125d3afbb24a10eb
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-12-19 15:35:28 +0800

    tools/run-phase-latency.py

---
+++ tools/run-phase-latency.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(sys.argv[0]), '../helper/'))
+from common import *
+
+import pandas as pd
+
+info = json5.load(open(os.path.join('.', 'info.json')))
+
+def work(t):
+    latency = pd.read_table(t + '-latency', delim_whitespace=True)
+    latency = latency[latency['Timestamp(ns)'] >= info['run-start-timestamp(ns)']].iloc[-1]
+    print(latency)
+print('Read')
+work('read')
+print('Insert')
+work('insert')
