6828510087a8ba0a1477b8550ce19722d30fa227
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-12-19 17:18:19 +0800

    plot/latency.py: bar plot

--- plot/latency.py
+++ plot/latency.py
@@ -8,12 +8,16 @@ if len(sys.argv) != 2:
 dir = sys.argv[1]
 
 import os
+sys.path.insert(0, os.path.join(os.path.dirname(sys.argv[0]), '../helper/'))
+import common
+
 import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from matplotlib.ticker import LogLocator
+from matplotlib.ticker import ScalarFormatter
 
 # Paper specific settings
 STANDARD_WIDTH = 17.8
@@ -29,91 +33,171 @@ mpl.rcParams.update({
     })
 plt.rcParams['axes.unicode_minus'] = False
 
-fig = plt.figure(dpi = 300, figsize = (cm_to_inch(DOUBLE_COL_WIDTH), cm_to_inch(4)))
+figure = plt.figure(dpi = 300, figsize = (cm_to_inch(DOUBLE_COL_WIDTH), cm_to_inch(8)))
 
-tests = [
-    {
-        'title': '(a) YCSB-A\' hotspot-5% insert latency',
-        'workload': 'read_0.5_insert_0.5_hotspot0.05_110GB',
-        'workload-name': 'read-0.5-insert-0.5-hotspot',
-        'operation': 'read',
-    },
-    {
-        'title': '(b) YCSB-A\' hotspot-5% read latency',
-        'workload': 'read_0.5_insert_0.5_hotspot0.05_110GB',
-        'workload-name': 'read-0.5-insert-0.5-hotspot',
-        'operation': 'insert',
-    },
-    {
-        'title': '(c) YCSB-C hotspot-5% read latency',
-        'workload': 'ycsbc_hotspot0.05_110GB',
-        'workload-name': 'ycsbc-hotspot',
-        'operation': 'read',
-    },
-]
 versions=[
     {
         'path': 'flush-stably-hot',
+        'pattern': '///',
         'color': plt.get_cmap('Set2')(0),
-        'marker': 'o',
     },
     {
         'path': 'rocksdb-fat',
+        'pattern': '\\\\\\',
         'color': plt.get_cmap('Set2')(1),
-        'marker':'^',
     },
     {
         'path': 'secondary-cache',
+        'pattern': 'XXX',
         'color': plt.get_cmap('Set2')(3),
-        'marker': 's',
     },
     {
         'path': 'rocksdb-sd',
+        'pattern': 'XXXXXXXXX',
         'color': plt.get_cmap('Set2')(2),
-        'marker':'x',
     },
 ]
 version_names = ['HotRAP', 'RocksDB-fat', 'RocksDB-secondary-cache', 'RocksDB(SD)']
 percentiles = [50, 60, 70, 80, 90, 95, 99, 99.9]
 
-gs = gridspec.GridSpec(1, len(tests))
+gs = gridspec.GridSpec(2, 4)
 bar_width = 1 / (len(versions) + 1)
 cluster_width = bar_width * len(versions)
 
-for (i, test) in enumerate(tests):
-    subfig = plt.subplot(gs[0, i])
-    ax = plt.gca()
+figs = [
+    {
+        'title': 'Average latency of get',
+        'subfig': plt.subplot(gs[0, 0]),
+        'ax': plt.gca(),
+        'xticks': ['RO', 'RW', 'WH'],
+        'operation': 'read',
+        'percentile': 'Average',
+    },
+    {
+        'title': '50% latency of get',
+        'subfig': plt.subplot(gs[0, 1]),
+        'ax': plt.gca(),
+        'xticks': ['RO', 'RW', 'WH'],
+        'operation': 'read',
+        'percentile': '50%',
+    },
+    {
+        'title': '99% tail latency of get',
+        'subfig': plt.subplot(gs[0, 2]),
+        'ax': plt.gca(),
+        'xticks': ['RO', 'RW', 'WH'],
+        'operation': 'read',
+        'percentile': '99%',
+    },
+    {
+        'title': '99.9% tail latency of get',
+        'subfig': plt.subplot(gs[0, 3]),
+        'ax': plt.gca(),
+        'xticks': ['RO', 'RW', 'WH'],
+        'operation': 'read',
+        'percentile': '99.9%',
+    },
+    {
+        'title': 'Average latency of insert',
+        'subfig': plt.subplot(gs[1, 0]),
+        'ax': plt.gca(),
+        'xticks': ['RW', 'WH'],
+        'operation': 'insert',
+        'percentile': 'Average',
+    },
+    {
+        'title': '50% tail latency of insert',
+        'subfig': plt.subplot(gs[1, 1]),
+        'ax': plt.gca(),
+        'xticks': ['RW', 'WH'],
+        'operation': 'insert',
+        'percentile': '50%',
+    },
+    {
+        'title': '99% tail latency of insert',
+        'subfig': plt.subplot(gs[1, 2]),
+        'ax': plt.gca(),
+        'xticks': ['RW', 'WH'],
+        'operation': 'insert',
+        'percentile': '99%',
+    },
+    {
+        'title': '99.9% tail latency of insert',
+        'subfig': plt.subplot(gs[1, 3]),
+        'ax': plt.gca(),
+        'xticks': ['RW', 'WH'],
+        'operation': 'insert',
+        'percentile': '99.9%',
+    }
+]
+
+workloads = [
+    {
+        'path': 'ycsbc_hotspot0.05_110GB',
+        'name': 'RO',
+    },
+    {
+        'path': 'read_0.75_insert_0.25_hotspot0.05_110GB',
+        'name': 'RW',
+    },
+    {
+        'path': 'read_0.5_insert_0.5_hotspot0.05_110GB',
+        'name': 'WH',
+    },
+]
+
+for workload in workloads:
+    workload_dir = os.path.join(dir, workload['path'])
+    data_dir = os.path.join(workload_dir, 'flush-stably-hot')
+    warmup_finish_progress = common.warmup_finish_progress(data_dir)
+    for (version_idx, version) in enumerate(versions):
+        data_dir = os.path.join(workload_dir, version['path'])
+        warmup_finish_ts = common.progress_to_timestamp(data_dir, warmup_finish_progress)
+        for fig in figs:
+            try:
+                pivot = fig['xticks'].index(workload['name'])
+            except ValueError:
+                continue
+            # print(workload['path'], version['path'])
+            x = pivot - cluster_width / 2 + bar_width / 2 + version_idx * bar_width
+            path = os.path.join(data_dir, fig['operation'] + '-latency')
+            if version_idx == 0:
+                latency = pd.read_table(path, delim_whitespace=True)
+                latency = latency[latency['Timestamp(ns)'] >= warmup_finish_ts].iloc[-1]
+                value = latency[fig['percentile']]
+            else:
+                # Temporarily
+                latency = pd.read_table(path, delim_whitespace=True, names=['percentile', 'latency(ns)'])
+                latency = latency.set_index('percentile')['latency(ns)']
+                # print(latency)
+                if fig['percentile'] == 'Average':
+                    value = latency['Average']
+                elif fig['percentile'] == '50%':
+                    value = latency['0.5']
+                elif fig['percentile'] == '99%':
+                    value = latency['0.99']
+                elif fig['percentile'] == '99.9%':
+                    value = latency['0.999']
+                else:
+                    assert False
+            fig['ax'].bar(x, value, width=bar_width, hatch=version['pattern'], color=version['color'], edgecolor='black', linewidth=0.5)
+
+for (i, fig) in enumerate(figs):
+    subfig = fig['subfig']
+    ax = fig['ax']
     ax.set_axisbelow(True)
     ax.grid(axis='y')
 
-    workload = test['workload']
-    operation = test['operation']
-    for (version_idx, version) in enumerate(versions):
-        data_dir = os.path.join(dir, workload, version['path'])
-        path = os.path.join(data_dir, operation + '-latency-cdf')
-        cdf = pd.read_table(path, delim_whitespace=True, names=['latency', 'cdf'])
-        latencies = np.array(cdf['latency'])
-        cdf = np.array(cdf['cdf'])
-        x = []
-        y = []
-        for percentile in percentiles:
-            x.append(str(percentile))
-            y.append(latencies[np.searchsorted(cdf, percentile / 100)])
-        x.append('max')
-        y.append(latencies[-1])
-        ax.plot(x, y, color=version['color'], marker=version['marker'], markerfacecolor='none', linewidth=1, markersize=4)
-    plt.xticks(fontsize=8)
-    plt.yscale('log')
-    if i == 1:
-        plt.yticks([1e5, 1e6, 1e7], fontsize=8)
-        ax.yaxis.set_minor_locator(LogLocator(base=10, subs=np.arange(2, 10) * 0.1, numticks=233))
-    else:
-        plt.yticks(fontsize=8)
-    subfig.text(0.5, -0.27, 'Percentiles (%)', fontsize=6, ha='center', va='center', transform=subfig.transAxes)
-    plt.xlabel(test['title'], labelpad = 8, fontsize=8)
+    formatter = ScalarFormatter(useMathText=True)
+    formatter.set_powerlimits((-3, 4))
+    ax.yaxis.set_major_formatter(formatter)
+    ax.yaxis.get_offset_text().set_fontsize(8)
+
+    ax.set_xticks(list(range(0, len(fig['xticks']))), fig['xticks'], fontsize=8)
+    ax.set_xlabel(fig['title'], labelpad=8, fontsize=8)
     if i == 0:
-        plt.ylabel('Latency (ns)', fontsize=8)
-fig.legend(version_names, fontsize=8, ncol=len(versions), loc='center', bbox_to_anchor=(0.5, 0.99))
+        ax.set_ylabel('Latency (ns)', fontsize=8)
+figure.legend(version_names, fontsize=8, ncol=len(version_names), loc='center', bbox_to_anchor=(0.5, 0.99))
 plt.tight_layout()
 pdf_path = os.path.join(dir, 'latency.pdf')
 plt.savefig(pdf_path, bbox_inches='tight', pad_inches=0.01)
