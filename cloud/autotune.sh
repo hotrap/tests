5056fafb8eb38a67b678bd270487d81f798e1887
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-01-23 14:53:08 +0800

    cloud/autotune.sh: hotspotshifting, promote-accessed, no-retain, no-promote-by-compaction

--- cloud/autotune.sh
+++ cloud/autotune.sh
@@ -28,7 +28,7 @@ function run-hotrap {
 	workload=$1
 	version=$2
 	IP=$3
-	./checkout-hotrap $user $IP promote-stably-hot
+	./checkout-hotrap $user $IP $version
 	# Reserve 330MB for VisCnts
 	ssh $user@$IP -o ServerAliveInterval=60 "source ~/.profile && cd tests/workloads && ./test-hotrap-110GB.sh ../config/$workload ../../data/$workload/$version 9.67GB 5.5GB 330MB \"--enable_dynamic_vc_param_in_lsm --enable_dynamic_only_vc_phy_size\""
 	rsync -zPrt -e ssh $user@$IP:~/data/$workload $output_dir/
@@ -36,6 +36,10 @@ function run-hotrap {
 }
 
 for workload in "${workloads[@]}"; do
-	cloud-run run-hotrap $workload promote-stably-hot-autotune
+	cloud-run run-hotrap $workload promote-stably-hot
 done
+cloud-run run-hotrap "ycsbc_hotspotshifting0.05_110GB_165GB" promote-stably-hot
+cloud-run run-hotrap "ycsbc_uniform_110GB_165GB" promote-accessed
+cloud-run run-hotrap "read_0.75_insert_0.25_hotspot0.05_110GB_165GB" no-retain
+cloud-run run-hotrap "read_0.75_insert_0.25_hotspot0.05_110GB_165GB" no-promote-by-compaction
 wait
