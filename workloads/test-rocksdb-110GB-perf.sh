b6c73fb7f14738203dddbce563e32d1b31d6acbd
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-01-27 18:37:07 +0800

    Don't load when perf

---
+++ workloads/test-rocksdb-110GB-perf.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+if [[ $# < 3 || $# > 4 ]]; then
+	echo Usage: $0 workload-file output-dir sd-size [extra-kvexe-args]
+	exit 1
+fi
+set -e
+set -o pipefail
+# To make set -e take effect if the output dir does not exists
+mkdir -p $2
+res="$(ls -A $2)"
+if [ "$res" ]; then
+        echo "$2" is not empty!
+        exit 1
+fi
+workload_file=$(realpath $1)
+DIR=$(realpath "$2")
+sd_size=$(humanfriendly --parse-size=$3)
+max_memory=$(humanfriendly --parse-size=1.1GB)
+extra_kvexe_args="$4"
+cd "$(dirname $0)"
+workspace=$(realpath ../..)
+kvexe_dir=$workspace/kvexe-rocksdb/build/
+
+ulimit -n 100000
+../helper/exe-while.sh $DIR bash -c "systemd-run --user -E LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 --scope -p MemoryMax=$max_memory perf record --call-graph=fp $kvexe_dir/rocksdb-kvexe --run --switches=0x1 --num_threads=16 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=67108864 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,1000000000000}}\" $extra_kvexe_args 2>> $DIR/log.txt"
+bash ../helper/rocksdb-data.sh "$DIR"
