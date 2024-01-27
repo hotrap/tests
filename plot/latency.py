60d6064d37c01020aab90ce59c1f57edb793c24c
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-01-27 19:14:40 +0000

    plot/latency.py: Mutant and PrismDB

--- plot/latency.py
+++ plot/latency.py
@@ -34,29 +34,68 @@ plt.rcParams['axes.unicode_minus'] = False
 
 figure = plt.figure(dpi = 300, figsize = (cm_to_inch(DOUBLE_COL_WIDTH), cm_to_inch(4)))
 
+xticks = ['RO', 'RW', 'WH']
+workloads = [
+    {
+        'path': 'ycsbc_hotspot0.05_110GB',
+    },
+    {
+        'path': 'read_0.75_insert_0.25_hotspot0.05_110GB',
+    },
+    {
+        'path': 'read_0.5_insert_0.5_hotspot0.05_110GB',
+    },
+]
+workloads_prismdb_mutant = [
+    {
+        'path': 'workload_110GB_ycsbc_hotspot0.05',
+    },
+    {
+        'path': 'workload_110GB_wr_hotspot0.05',
+    },
+    {
+        'path': 'workload_110GB_wh_hotspot0.05',
+    },
+]
 versions=[
     {
         'path': 'promote-stably-hot',
         'pattern': '///',
         'color': plt.get_cmap('Set2')(0),
+        'workloads': workloads,
     },
     {
         'path': 'rocksdb-fat',
         'pattern': '\\\\\\',
         'color': plt.get_cmap('Set2')(1),
+        'workloads': workloads,
     },
     {
         'path': 'secondary-cache',
         'pattern': 'XXX',
         'color': plt.get_cmap('Set2')(3),
+        'workloads': workloads,
     },
     {
         'path': 'rocksdb-sd',
         'pattern': 'XXXXXXXXX',
         'color': plt.get_cmap('Set2')(2),
+        'workloads': workloads,
     },
+    {
+        'path': 'mutant',
+        'pattern': '---',
+        'color': plt.get_cmap('Set2')(4),
+        'workloads': workloads_prismdb_mutant,
+    },
+    {
+        'path': 'prismdb',
+        'pattern': '---\\\\\\\\\\\\',
+        'color': plt.get_cmap('Set2')(5),
+        'workloads': workloads_prismdb_mutant,
+    }
 ]
-version_names = ['HotRAP', 'RocksDB-fat', 'RocksDB-secondary-cache', 'RocksDB(SD)']
+version_names = ['HotRAP', 'RocksDB-fat', 'RocksDB-secondary-cache', 'RocksDB(SD)', 'Mutant', 'PrismDB']
 
 gs = gridspec.GridSpec(1, 3)
 bar_width = 1 / (len(versions) + 1)
@@ -67,62 +106,49 @@ figs = [
         'title': '50% latency of get',
         'subfig': plt.subplot(gs[0, 0]),
         'ax': plt.gca(),
-        'xticks': ['RO', 'RW', 'WH'],
-        'operation': 'read',
         'percentile': '50%',
     },
     {
-        'title': '99% tail latency of get',
+        'title': '99% tail latency of get (log scale)',
         'subfig': plt.subplot(gs[0, 1]),
         'ax': plt.gca(),
-        'xticks': ['RO', 'RW', 'WH'],
-        'operation': 'read',
         'percentile': '99%',
+        'yscale': 'log',
     },
     {
-        'title': '99.9% tail latency of get',
+        'title': '99.9% tail latency of get (log scale)',
         'subfig': plt.subplot(gs[0, 2]),
         'ax': plt.gca(),
-        'xticks': ['RO', 'RW', 'WH'],
-        'operation': 'read',
         'percentile': '99.9%',
+        'yscale': 'log',
     },
 ]
 
-workloads = [
-    {
-        'path': 'ycsbc_hotspot0.05_110GB',
-        'name': 'RO',
-    },
-    {
-        'path': 'read_0.75_insert_0.25_hotspot0.05_110GB',
-        'name': 'RW',
-    },
-    {
-        'path': 'read_0.5_insert_0.5_hotspot0.05_110GB',
-        'name': 'WH',
-    },
-]
-
+warmup_finish_progress = {}
 for workload in workloads:
     workload_dir = os.path.join(dir, workload['path'])
     data_dir = os.path.join(workload_dir, 'promote-stably-hot')
-    warmup_finish_progress = common.warmup_finish_progress(data_dir)
-    for (version_idx, version) in enumerate(versions):
-        data_dir = os.path.join(workload_dir, version['path'])
-        warmup_finish_ts = common.progress_to_timestamp(data_dir, warmup_finish_progress)
-        for fig in figs:
-            try:
-                pivot = fig['xticks'].index(workload['name'])
-            except ValueError:
-                continue
-            x = pivot - cluster_width / 2 + bar_width / 2 + version_idx * bar_width
-            path = os.path.join(data_dir, fig['operation'] + '-latency')
-            latency = pd.read_table(path, delim_whitespace=True)
-            latency = latency[latency['Timestamp(ns)'] >= warmup_finish_ts].iloc[-1]
-            value = latency[fig['percentile']]
-            fig['ax'].bar(x, value, width=bar_width, hatch=version['pattern'], color=version['color'], edgecolor='black', linewidth=0.5)
-            # print(workload['name'], version['path'], fig['percentile'], value)
+    warmup_finish_progress[workload['path']] = common.warmup_finish_progress(data_dir)
+
+for (version_idx, version) in enumerate(versions):
+    for (pivot, workload) in enumerate(version['workloads']):
+        data_dir = os.path.join(dir, workload['path'], version['path'])
+        if version_idx < 4:
+            warmup_finish_ts = common.progress_to_timestamp(data_dir, warmup_finish_progress[workload['path']])
+            for fig in figs:
+                x = pivot - cluster_width / 2 + bar_width / 2 + version_idx * bar_width
+                path = os.path.join(data_dir, 'read-latency')
+                latency = pd.read_table(path, delim_whitespace=True)
+                latency = latency[latency['Timestamp(ns)'] >= warmup_finish_ts].iloc[-1]
+                value = latency[fig['percentile']]
+                fig['ax'].bar(x, value, width=bar_width, hatch=version['pattern'], color=version['color'], edgecolor='black', linewidth=0.5)
+                # print(workload['name'], version['path'], fig['percentile'], value)
+        else:
+            latency = pd.read_table(os.path.join(data_dir, 'last-10p-read-latency'), delim_whitespace=True)
+            for fig in figs:
+                x = pivot - cluster_width / 2 + bar_width / 2 + version_idx * bar_width
+                value = latency[fig['percentile']]
+                fig['ax'].bar(x, value, width=bar_width, hatch=version['pattern'], color=version['color'], edgecolor='black', linewidth=0.5)
 
 for (i, fig) in enumerate(figs):
     subfig = fig['subfig']
@@ -135,11 +161,16 @@ for (i, fig) in enumerate(figs):
     ax.yaxis.set_major_formatter(formatter)
     ax.yaxis.get_offset_text().set_fontsize(8)
 
-    ax.set_xticks(list(range(0, len(fig['xticks']))), fig['xticks'], fontsize=8)
+    ax.set_xticks(list(range(0, len(xticks))), xticks, fontsize=8)
     ax.set_xlabel(fig['title'], labelpad=8, fontsize=8)
+    if 'yscale' in fig:
+        ax.set_yscale(fig['yscale'])
     if i == 0:
         ax.set_ylabel('Latency (ns)', fontsize=8)
-figure.legend(version_names, fontsize=8, ncol=len(version_names), loc='center', bbox_to_anchor=(0.5, 0.99))
+handles = []
+for version in versions:
+    handles.append(mpl.patches.Patch(facecolor=version['color'], hatch=version['pattern'], edgecolor='black', linewidth=0.5))
+figure.legend(handles=handles, labels=version_names, fontsize=8, ncol=len(version_names), loc='center', bbox_to_anchor=(0.5, 0.99))
 plt.tight_layout()
 pdf_path = os.path.join(dir, 'latency.pdf')
 plt.savefig(pdf_path, bbox_inches='tight', pad_inches=0.01)
