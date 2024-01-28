d5b26da7b9185cefcd5d9725b7b2c0c9f4b5d130
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-01-28 09:10:52 +0000

    Don't hard limit the memory usage of hotrap

--- workloads/test-hotrap-110GB.sh
+++ workloads/test-hotrap-110GB.sh
@@ -14,12 +14,11 @@ DIR=$(realpath "$2")
 sd_size=$(humanfriendly --parse-size=$3)
 max_hot_set_size=$(humanfriendly --parse-size=$4)
 max_viscnts_size=$(humanfriendly --parse-size=$5)
-max_memory=$(humanfriendly --parse-size=1.1GB)
 extra_kvexe_args="$6"
 cd "$(dirname $0)"
 workspace=$(realpath ../..)
 kvexe_dir=$workspace/kvexe/build/
 
 ulimit -n 100000
-../helper/exe-while.sh $DIR bash -c "systemd-run --user -E LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 --scope -p MemoryMax=$max_memory $kvexe_dir/rocksdb-kvexe --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --max_viscnts_size=$max_viscnts_size --switches=0x1 --num_threads=16 --max_background_jobs=4 --block_size=16384 --max_bytes_for_level_base=67108864 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,100000000000}}\" --viscnts_path=$workspace/testdb/viscnts $extra_kvexe_args 2>> $DIR/log.txt"
+../helper/exe-while.sh $DIR bash -c "LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 $kvexe_dir/rocksdb-kvexe --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --max_viscnts_size=$max_viscnts_size --switches=0x1 --num_threads=16 --max_background_jobs=4 --block_size=16384 --max_bytes_for_level_base=67108864 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/sd,$sd_size},{$workspace/testdb/cd,100000000000}}\" --viscnts_path=$workspace/testdb/viscnts $extra_kvexe_args 2>> $DIR/log.txt"
 bash ../helper/hotrap-data.sh "$DIR"
