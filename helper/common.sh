df70d20073fe3755db4928675adb760e776273c2
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-01-30 12:41:08 +0000

    rocksdb -DWITH_TBB=on

--- helper/common.sh
+++ helper/common.sh
@@ -1,7 +1,7 @@
 function build_rocksdb {
 	mkdir -p build
 	cd build
-	cmake .. -DCMAKE_BUILD_TYPE=None -DCMAKE_C_FLAGS="-Wall -O3" -DCMAKE_CXX_FLAGS="-Wall -O3" -DUSE_RTTI=true -DFAIL_ON_WARNINGS=OFF
+	cmake .. -DCMAKE_BUILD_TYPE=None -DCMAKE_C_FLAGS="-Wall -O3" -DCMAKE_CXX_FLAGS="-Wall -O3" -DUSE_RTTI=true -DFAIL_ON_WARNINGS=OFF -DWITH_TBB=on
 	make -j$(nproc) rocksdb-shared
 	cd ..
 }
