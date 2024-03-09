20eb55677c67accaac2c19eedb487a3d4f5948b8
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-03-09 06:50:20 +0000

    nofail

--- cloud/helper/aws-hdd.sh
+++ cloud/helper/aws-hdd.sh
@@ -2,14 +2,14 @@ sudo mkfs.ext4 /dev/nvme1n1
 sudo mkdir /mnt/sd
 sudo mount /dev/nvme1n1 /mnt/sd
 sudo chown -R $USER:$USER /mnt/sd
-sudo bash -c "echo \"/dev/nvme1n1 /mnt/sd ext4 defaults 0 0\" >> /etc/fstab"
+sudo bash -c "echo \"/dev/nvme1n1 /mnt/sd ext4 defaults,nofail 0 0\" >> /etc/fstab"
 
 sudo mkfs.ext4 /dev/nvme2n1
 sudo mkdir /mnt/fd
 sudo mount /dev/nvme2n1 /mnt/fd
 sudo chown -R $USER:$USER /mnt/fd
 mkdir -p /mnt/fd/{db,fd,viscnts}     
-sudo bash -c "echo \"/dev/nvme2n1 /mnt/fd ext4 defaults 0 0\" >> /etc/fstab"
+sudo bash -c "echo \"/dev/nvme2n1 /mnt/fd ext4 defaults,nofail 0 0\" >> /etc/fstab"
 
 cd ~
 mkdir testdb
--- cloud/helper/aws.sh
+++ cloud/helper/aws.sh
@@ -3,7 +3,7 @@ sudo mkdir /mnt/fd
 sudo mount /dev/nvme1n1 /mnt/fd
 sudo chown $USER:$USER /mnt/fd
 mkdir -p /mnt/fd/{db,fd,viscnts}     
-sudo bash -c "echo \"/dev/nvme1n1 /mnt/fd ext4 defaults 0 0\" >> /etc/fstab"
+sudo bash -c "echo \"/dev/nvme1n1 /mnt/fd ext4 defaults,nofail 0 0\" >> /etc/fstab"
 
 cd ~
 mkdir testdb
