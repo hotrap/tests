88ba2758fc3c863721dcb260b35bf653f9abc645
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-04-09 15:44:33 +0800

    plot/general.py: tooltip

--- plot/general.py
+++ plot/general.py
@@ -6,13 +6,14 @@ def print_help():
     print(sys.argv[0] + ' [-o <outputfile>] [-t line|scatter] [--xlable=] [--ylabel=]')
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'ho:t:', ['xlabel=', 'ylabel='])
+    opts, args = getopt.getopt(sys.argv[1:], 'ho:t:i', ['xlabel=', 'ylabel='])
 except getopt.GetoptError:
     print_help()
     exit(1)
 
 pdf_path = None
 chart_type = 'line'
+interactive = False
 xlabel = None
 ylabel = None
 for opt, arg in opts:
@@ -23,6 +24,8 @@ for opt, arg in opts:
         pdf_path = arg
     elif opt == '-t':
         chart_type = arg
+    elif opt == '-i':
+        interactive = True
     elif opt == '--xlabel':
         xlabel = arg
     elif opt == '--ylabel':
@@ -53,34 +56,46 @@ plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 legends = None
 if xlabel is None:
     header = shlex.split(sys.stdin.readline())
+    n = len(header)
     xlabel = header[0]
     legends = header[1:]
-    n = len(legends)
+    c = [[] for _ in range(0, n)]
 else:
-    n = 1
-x = []
-ys = []
-for _ in range(0, n):
-    ys.append([])
+    line = sys.stdin.readline().rstrip().split(' ')
+    n = len(line)
+    c = [[v] for v in line]
 for line in sys.stdin:
     line = line.rstrip().split(' ')
-    x.append(float(line[0]))
+    assert len(line) == n
     for i in range(0, n):
-        ys[i].append(float(line[i+1]))
+        c[i].append(line[i])
+
+fig, ax = plt.subplots()
 if chart_type == 'line':
-    for y in ys:
-        plt.plot(x, y)
+    x = [float(v) for v in c[0]]
+    for y in c[1:]:
+        y = [float(v) for v in y]
+        ax.plot(x, y)
 else:
     assert chart_type == 'scatter'
-    assert len(ys) == 1
-    plt.scatter(x, ys[0])
-plt.xlabel(xlabel, fontdict=fonten)
+    assert len(c) == 3
+    x = [float(v) for v in c[0]]
+    y = [float(v) for v in c[1]]
+    labels = c[2]
+    scatter = ax.scatter(x, y)
+ax.set_xlabel(xlabel, fontdict=fonten)
 if ylabel is not None:
-    plt.ylabel(ylabel, fontdict=fonten)
+    ax.set_ylabel(ylabel, fontdict=fonten)
 if legends is not None:
     plt.legend(legends, prop={'size': fontsize})
 if pdf_path is not None:
     plt.savefig(pdf_path)
     print('Plot saved to ' + pdf_path)
-if 'DISPLAY' in os.environ:
+if interactive:
+    assert chart_type == 'scatter'
+    import mpld3
+    tooltip = mpld3.plugins.PointLabelTooltip(scatter, labels=labels)
+    mpld3.plugins.connect(fig, tooltip)
+    mpld3.show()
+elif 'DISPLAY' in os.environ:
 	plt.show()
