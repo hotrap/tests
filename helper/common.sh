2216e18591154c475e45d9a351d3274f123370a5
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-04-10 17:07:48 +0800

    RelWithDebInfo

--- helper/common.sh
+++ helper/common.sh
@@ -1,7 +1,7 @@
 function build_rocksdb {
 	mkdir -p build
 	cd build
-	cmake .. -DCMAKE_BUILD_TYPE=None -DCMAKE_C_FLAGS="-Wall -O3" -DCMAKE_CXX_FLAGS="-Wall -O3" -DUSE_RTTI=true -DFAIL_ON_WARNINGS=OFF -DWITH_TBB=on
+	cmake .. -DCMAKE_BUILD_TYPE=None -DCMAKE_C_FLAGS="-Wall -O3 -g" -DCMAKE_CXX_FLAGS="-Wall -O3 -g" -DUSE_RTTI=true -DFAIL_ON_WARNINGS=OFF -DWITH_TBB=on
 	make -j$(nproc) rocksdb-shared
 	cd ..
 }
@@ -36,7 +36,7 @@ function build_kvexe_rocksdb {
 	mkdir -p build
 	cd build
 	workspace=$(realpath ../..)
-	cmake .. -DCMAKE_BUILD_TYPE=Release -DROCKSDB_INCLUDE=$workspace/rocksdb/include -DROCKSDB_LIB=$workspace/rocksdb/build
+	cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DROCKSDB_INCLUDE=$workspace/rocksdb/include -DROCKSDB_LIB=$workspace/rocksdb/build
 	make
 	cd ..
 }
