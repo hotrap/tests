4128dd9c65b019d7798056497f3af35b8a8a5fb8
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-04-13 19:12:33 +0800

    helper/process_trace.sh

---
+++ helper/process_trace.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if [ ! $2 ]; then
+	echo Usage: $0 trace-file output-dir
+	exit 1
+fi
+set -e
+trace_file=$1
+output_dir=$2
+output_prefix=$output_dir/$(basename $trace_file)
+target_db_size=110000000000
+max_num_run_op=100000000
+~/tests/helper/twitter-to-plain $output_prefix $target_db_size $max_num_run_op < $trace_file | tee $output_prefix.log
+db_size=$(jq -r ".\"db-size\"" < $output_prefix.json)
+if [ $db_size -lt $target_db_size ]; then
+	~/tests/helper/augment-trace $output_prefix $max_num_run_op $((($target_db_size + $db_size - 1) / $db_size))
+fi
