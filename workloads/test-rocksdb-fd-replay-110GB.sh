2151c2b3f4dcd0cd273b617174d62f5fbaa7b815
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-04-13 19:00:51 +0800

    Trivial

--- workloads/test-rocksdb-fd-replay-110GB.sh
+++ workloads/test-rocksdb-fd-replay-110GB.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ $# < 3 || $# > 4 ]]; then
-	echo Usage: $0 trace-file-load trace-file-run output-dir [switches]
+	echo Usage: $0 trace-file-load trace-file-run output-dir [extra-kvexe-args]
 	exit 1
 fi
 set -e
@@ -15,16 +15,12 @@ fi
 trace_file_load=$(realpath $1)
 trace_file_run=$(realpath $2)
 DIR=$(realpath "$3")
-if [ $4 ]; then
-	switches=$4
-else
-	switches=0x1
-fi
+extra_kvexe_args="$4"
 workspace=$(realpath $(dirname $0)/../..)
 kvexe_dir=$workspace/kvexe-rocksdb/build/
 
 ulimit -n 100000
 cd $DIR
-$kvexe_dir/rocksdb-kvexe --load --format=plain-length-only --switches=$switches --num_threads=16 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=67108864 --enable_fast_process --db_path=$workspace/testdb/db/ --db_paths="{{$workspace/testdb/fd,1000000000000}}" --trace=$trace_file_load 2>> log.txt
-$workspace/tests/helper/exe-while.sh . bash -c "$kvexe_dir/rocksdb-kvexe --run --format=plain-length-only --switches=$switches --num_threads=16 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=67108864 --enable_fast_process --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,1000000000000}}\" --trace=$trace_file_run 2>> log.txt"
+$kvexe_dir/rocksdb-kvexe --load --format=plain-length-only --num_threads=16 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=67108864 --enable_fast_process --db_path=$workspace/testdb/db/ --db_paths="{{$workspace/testdb/fd,1000000000000}}" --trace=$trace_file_load 2>> log.txt
+$workspace/tests/helper/exe-while.sh . bash -c "$kvexe_dir/rocksdb-kvexe --run --format=plain-length-only --switches=0x1 --num_threads=16 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=67108864 --enable_fast_process --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,1000000000000}}\" --trace=$trace_file_run 2>> log.txt"
 bash $workspace/tests/helper/rocksdb-data.sh .
--- workloads/test-secondary-cache-replay-110GB.sh
+++ workloads/test-secondary-cache-replay-110GB.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ $# < 3 || $# > 4 ]]; then
-	echo Usage: $0 trace-file-load trace-file-run output-dir [switches]
+	echo Usage: $0 trace-file-load trace-file-run output-dir [extra-kvexe-args]
 	exit 1
 fi
 set -e
@@ -13,16 +13,12 @@ if [ "$(ls -A $DIR)" ]; then
         echo "$3" is not empty!
         exit 1
 fi
-if [ $4 ]; then
-	switches=$4
-else
-	switches=0x1
-fi
-cd "$(dirname $0)"
-workspace=$(realpath ../..)
+extra_kvexe_args="$4"
+workspace=$(realpath $(dirname $0)/../..)
 kvexe_dir=$workspace/kvexe-secondary-cache/build/
 
 ulimit -n 100000
-$kvexe_dir/rocksdb-kvexe --load --format=plain-length-only --switches=$switches --num_threads=16 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=335544320 --secondary_cache_size=6000000000 --enable_fast_process --db_path=$workspace/testdb/db/ --db_paths="{{$workspace/testdb/fd,4030000000},{$workspace/testdb/sd,1000000000000}}" --trace=$trace_file_load 2>> $DIR/log.txt
-../helper/exe-while.sh $DIR bash -c "$kvexe_dir/rocksdb-kvexe --run --format=plain-length-only --switches=$switches --num_threads=16 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=335544320 --secondary_cache_size=6000000000 --enable_fast_process --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,4030000000},{$workspace/testdb/sd,1000000000000}}\" --trace=$trace_file_run 2>> $DIR/log.txt"
-bash ../helper/rocksdb-data.sh "$DIR"
+cd $DIR
+$kvexe_dir/rocksdb-kvexe --load --format=plain-length-only --num_threads=16 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=335544320 --secondary_cache_size=6000000000 --enable_fast_process --db_path=$workspace/testdb/db/ --db_paths="{{$workspace/testdb/fd,4030000000},{$workspace/testdb/sd,1000000000000}}" --trace=$trace_file_load 2>> log.txt
+$workspace/tests/helper/exe-while.sh . bash -c "$kvexe_dir/rocksdb-kvexe --run --format=plain-length-only --switches=0x1 --num_threads=16 --max_background_jobs=4 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=335544320 --secondary_cache_size=6000000000 --enable_fast_process --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,4030000000},{$workspace/testdb/sd,1000000000000}}\" --trace=$trace_file_run 2>> log.txt"
+bash $workspace/tests/helper/rocksdb-data.sh .
