c81e31711eee4c7a4c0c7b75276de8bd9fe4326e
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-04-20 16:53:04 +0800

    Compress load trace and run trace

--- helper/process-trace.sh
+++ helper/process-trace.sh
@@ -10,8 +10,11 @@ output_prefix=$output_dir/$(basename $trace_file)
 target_db_size=110000000000
 max_num_run_op=500000000
 ~/tests/helper/twitter-to-plain $output_prefix $target_db_size $max_num_run_op < $trace_file | tee $output_prefix.log
+zstdmt $output_prefix-load $output_prefix-run
 db_size=$(jq -r ".\"db-size\"" < $output_prefix.json)
 if [ $db_size -lt $target_db_size ]; then
 	multiple=$((($target_db_size + $db_size - 1) / $db_size))
 	~/tests/helper/augment-trace $output_prefix $max_num_run_op $multiple
+	prefix=$output_prefix-${multiple}x
+	zstdmt $prefix-load $prefix-run
 fi
