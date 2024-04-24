e6538a79439f5d8c0c61366cd633c8bba39aee60
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-04-24 15:20:08 +0800

    Precisely augment

--- helper/src/twitter-to-plain/src/main.rs
+++ helper/src/twitter-to-plain/src/main.rs
@@ -16,6 +16,14 @@ enum Operation<'a> {
     // value size == -1 if it's getting a non-existing key
     Read(&'a str, isize),
 }
+impl<'a> Operation<'a> {
+    fn key(&'a self) -> &'a str {
+        match self {
+            Self::Insert(key, _) => key,
+            Self::Read(key, _) => key,
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut args = env::args();
@@ -28,7 +36,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             "Invalid arguments",
         )));
     }
-    let prefix = args.next().unwrap();
+    let mut prefix = args.next().unwrap();
     let target_db_size: usize = args.next().unwrap().parse().unwrap();
     let max_num_run_op: usize = args.next().unwrap().parse().unwrap();
     assert!(max_num_run_op > 0);
@@ -46,6 +54,27 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut run = SerdeQueue::new();
     let mut load = HashMap::<String, (usize, usize)>::new();
     let mut timestamp = 0;
+    fn add_to_load(
+        op: Operation,
+        timestamp: &mut usize,
+        load: &mut HashMap<String, (usize, usize)>,
+    ) {
+        match op {
+            Operation::Insert(key, value_size) => {
+                *timestamp += 1;
+                load.entry(key.to_owned())
+                    .and_modify(|v| *v = (value_size, *timestamp))
+                    .or_insert((value_size, *timestamp));
+            }
+            Operation::Read(key, value_size) => {
+                if value_size > 0 {
+                    assert!(load
+                        .insert(key.to_owned(), (value_size as usize, 0))
+                        .is_none());
+                }
+            }
+        }
+    }
     // Useful unstable feature: closure_lifetime_binder
     fn add<'a, 'b>(
         run: &'a mut SerdeQueue,
@@ -55,26 +84,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         load: &mut HashMap<String, (usize, usize)>,
     ) {
         if run.len() == max_num_run_op {
-            match run.pop().unwrap().unwrap() {
-                Operation::Insert(key, value_size) => {
-                    *timestamp += 1;
-                    load.entry(key.to_owned())
-                        .and_modify(|v| *v = (value_size, *timestamp))
-                        .or_insert((value_size, *timestamp));
-                }
-                Operation::Read(key, value_size) => {
-                    if value_size > 0 {
-                        assert!(load
-                            .insert(key.to_owned(), (value_size as usize, 0))
-                            .is_none());
-                    }
-                }
-            }
+            add_to_load(run.pop().unwrap().unwrap(), timestamp, load);
         }
         run.push(&op).unwrap();
     }
 
-    let mut kv = HashMap::<String, usize>::new();
+    struct KeyInfo {
+        value_size: usize,
+        augment: usize,
+    }
+    let mut keys = HashMap::<String, KeyInfo>::new();
     let mut db_size = 0;
 
     let mut reader = BufReader::new(io::stdin());
@@ -103,9 +122,15 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let value_size = value_size.parse().expect(&nr.to_string());
         if read_op.contains(op) {
-            if !kv.contains_key(key) {
+            if !keys.contains_key(key) {
                 if value_size > 0 {
-                    kv.insert(key.to_owned(), value_size);
+                    keys.insert(
+                        key.to_owned(),
+                        KeyInfo {
+                            value_size,
+                            augment: 1,
+                        },
+                    );
                     db_size += key.len() + value_size;
                     add(
                         &mut run,
@@ -133,11 +158,17 @@ fn main() -> Result<(), Box<dyn Error>> {
                 );
             }
         } else if insert_op.contains(op) {
-            if let Some(old_value_len) = kv.get_mut(key) {
-                db_size = db_size - *old_value_len + value_size;
-                *old_value_len = value_size;
+            if let Some(old_info) = keys.get_mut(key) {
+                db_size = db_size - old_info.value_size + value_size;
+                old_info.value_size = value_size;
             } else {
-                kv.insert(key.to_owned(), value_size);
+                keys.insert(
+                    key.to_owned(),
+                    KeyInfo {
+                        value_size,
+                        augment: 1,
+                    },
+                );
                 db_size += key.len() + value_size;
             }
             add(
@@ -158,28 +189,116 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
+    let mut stats_writer =
+        BufWriter::new(File::create(prefix.clone() + ".json").unwrap());
+    writeln!(&mut stats_writer, "{{\n\t\"db-size\": {},", db_size).unwrap();
+
+    let mut augment_prefix = Vec::new();
+    let mut num_prefix_digits = 0;
+    let mut default_augment = 1;
+    if db_size < target_db_size {
+        default_augment = (target_db_size + db_size - 1) / db_size;
+
+        let mut n = default_augment - 1;
+        while n > 0 {
+            num_prefix_digits += 1;
+            n /= 10;
+        }
+        let db_size = db_size + keys.len() * num_prefix_digits;
+        if db_size >= target_db_size {
+            num_prefix_digits = 0;
+        } else {
+            let mut augmented_db_size = db_size * (default_augment - 1);
+            assert!(augmented_db_size + db_size >= target_db_size);
+            while augmented_db_size >= target_db_size {
+                default_augment -= 1;
+                augmented_db_size -= db_size;
+            }
+            for (key, info) in keys.iter_mut() {
+                if augmented_db_size >= target_db_size {
+                    info.augment = default_augment - 1;
+                } else {
+                    info.augment = default_augment;
+                    augmented_db_size +=
+                        num_prefix_digits + key.len() + info.value_size;
+                }
+            }
+            writeln!(
+                &mut stats_writer,
+                "\t\"augment\": {},\n\t\"augmented-db-size\": {},",
+                default_augment, augmented_db_size
+            )
+            .unwrap();
+            prefix.push('-');
+            prefix += &default_augment.to_string();
+            prefix.push('x');
+            for i in 0..default_augment {
+                augment_prefix.push(format!("{:01$}", i, num_prefix_digits));
+            }
+
+            let mut augmented_num_run_op = 0;
+            for op in run.iter::<Operation>() {
+                if let Some(info) = keys.get(op.key()) {
+                    augmented_num_run_op += info.augment;
+                } else {
+                    augmented_num_run_op += default_augment;
+                }
+            }
+            while augmented_num_run_op > max_num_run_op {
+                let op: Operation = run.pop().unwrap().unwrap();
+                if let Some(info) = keys.get(op.key()) {
+                    augmented_num_run_op -= info.augment;
+                    add_to_load(op, &mut timestamp, &mut load);
+                } else {
+                    augmented_num_run_op -= default_augment;
+                }
+            }
+        }
+    }
+    if augment_prefix.len() == 0 {
+        augment_prefix.push(String::new());
+    }
+
     let mut run_writer =
         BufWriter::new(File::create(prefix.clone() + "-run").unwrap());
-    let num_run_op = run.len();
     let mut num_run_inserts: usize = 0;
     let mut num_reads: usize = 0;
     let mut num_empty_reads: usize = 0;
     while let Some(operation) = run.pop().unwrap() {
         match operation {
             Operation::Insert(key, value_size) => {
-                num_run_inserts += 1;
-                writeln!(&mut run_writer, "INSERT {} {}", key, value_size)
+                let augment = keys.get(key).unwrap().augment;
+                num_run_inserts += augment;
+                for i in 0..augment {
+                    writeln!(
+                        &mut run_writer,
+                        "INSERT {}{} {}",
+                        augment_prefix[i], key, value_size
+                    )
                     .unwrap();
+                }
             }
             Operation::Read(key, value_size) => {
-                num_reads += 1;
-                writeln!(&mut run_writer, "READ {}", &key).unwrap();
+                let augment = if let Some(info) = keys.get(key) {
+                    info.augment
+                } else {
+                    default_augment
+                };
+                num_reads += augment;
+                for i in 0..augment {
+                    writeln!(
+                        &mut run_writer,
+                        "READ {}{}",
+                        augment_prefix[i], &key
+                    )
+                    .unwrap();
+                }
                 if value_size > 0 {
                     assert!(load
                         .insert(key.to_owned(), (value_size as usize, 0))
                         .is_none());
                 } else if value_size == -1 {
-                    num_empty_reads += 1;
+                    num_empty_reads += augment;
                 }
             }
         }
@@ -189,27 +308,29 @@ fn main() -> Result<(), Box<dyn Error>> {
         BufWriter::new(File::create(prefix.clone() + "-load").unwrap());
     let mut load: Vec<(String, (usize, usize))> = load.drain().collect();
     load.sort_unstable_by(|a, b| a.1 .1.cmp(&b.1 .1));
-    let num_load_op = load.len();
+    let mut num_load_op = 0;
     let mut load_key_len = 0;
     let mut load_value_len = 0;
     for (key, (value_size, _)) in load {
-        writeln!(&mut load_writer, "INSERT {} {}", key, value_size).unwrap();
-        load_key_len += key.len();
-        load_value_len += value_size;
+        let augment = keys.get(&key).unwrap().augment;
+        num_load_op += augment;
+        load_key_len += (num_prefix_digits + key.len()) * augment;
+        load_value_len += value_size * augment;
+        for i in 0..augment {
+            writeln!(
+                &mut load_writer,
+                "INSERT {}{} {}",
+                augment_prefix[i], key, value_size
+            )
+            .unwrap();
+        }
     }
 
-    println!(
-        "Final DB size: {}\nSkipped: {}\nUnique keys: {}",
-        db_size,
-        skipped,
-        kv.len()
-    );
+    println!("Skipped: {}\nUnique keys: {}", skipped, keys.len());
 
     writeln!(
-        BufWriter::new(File::create(prefix + ".json").unwrap()),
-        "{{
-\t\"db-size\": {},
-\t\"num-load-op\": {},
+        &mut stats_writer,
+        "\t\"num-load-op\": {},
 \t\"num-run-op\": {},
 \t\"num-run-inserts\": {},
 \t\"num-reads\": {},
@@ -217,9 +338,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 \t\"load-avg-key-len\": {},
 \t\"load-avg-value-len\": {}
 }}",
-        db_size,
         num_load_op,
-        num_run_op,
+        num_run_inserts + num_reads,
         num_run_inserts,
         num_reads,
         num_empty_reads,
