bbe5e5a11ba45a0a10fe287670d8e881606f39a1
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-04-24 20:50:34 +0800

    analyze-plain.sh: if -s

--- helper/analyze-plain.sh
+++ helper/analyze-plain.sh
@@ -5,9 +5,21 @@ if [ ! $1 ]; then
 fi
 set -e
 output_prefix=$1
+mydir=$(dirname $0)
 
-$(dirname $0)/analyze-plain $output_prefix
+$mydir/analyze-plain $output_prefix
 source $output_prefix
-frawk "BEGIN{count=0}{if (\$1 > $db_size * 0.05) {count += 1}}END{print count / NR}" $output_prefix-write-size-since-last-write > $output_prefix-read-with-more-than-5p-write-size
-frawk "BEGIN{count=0}{if (\$1 > 512) {count += 1}}END{print count / NR}" $output_prefix-num-reads-since-last-read > $output_prefix-read-with-more-than-512-reads
-frawk "BEGIN{count=0}{if (\$1 > 64 * 1024 * 1024) {count += 1}}END{print count / NR}" $output_prefix-read-size-since-last-read > $output_prefix-read-with-more-than-64MiB-read-size
+
+function ratio-gt {
+	input=$1
+	output=$2
+	num=$3
+	if [ -s $input ]; then
+		frawk "BEGIN{count=0}{if (\$1 > $num) {count += 1}}END{print count / NR}" $input > $output
+	else
+		echo 0 > $output
+	fi
+}
+ratio-gt $output_prefix-write-size-since-last-write $output_prefix-read-with-more-than-5p-write-size "$db_size * 0.05"
+ratio-gt $output_prefix-num-reads-since-last-read $output_prefix-read-with-more-than-512-reads 512
+ratio-gt $output_prefix-read-size-since-last-read $output_prefix-read-with-more-than-64MiB-read-size "64 * 1024 * 1024"
