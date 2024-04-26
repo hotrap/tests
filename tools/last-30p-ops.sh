8d8fa4a28befbbaa4e53b433bf306e0c9876012a
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-04-26 05:00:54 +0000

    Fix last-30p-ops of replay results

--- tools/last-30p-ops.sh
+++ tools/last-30p-ops.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
-timestamp_70p=$(hjson-cli -j < info.json | jq -r ".\"run-70%-timestamp(ns)\"")
-timestamp_100p=$(hjson-cli -j < info.json | jq -r ".\"run-end-timestamp(ns)\"")
-awk "{
-	if (NR > 1 && $timestamp_70p <= \$1 && \$1 < $timestamp_100p)
-		print \$0
-}" < progress | awk '{
-	if (NR == 1) {
-		first_timestamp = $1;
-		first_progress = $2;
-	}
-}
-END {
-	print ($2 - first_progress) / (($1 - first_timestamp) / 1e9)
-}'
+set -e
+timestamp_100p=$(hjson-cli -j < info.json | jq -er ".\"run-end-timestamp(ns)\"")
+num_total_op=$(tail -n1 progress | cut -sd' ' -f2)
+if timestamp_70p=$(hjson-cli -j < info.json | jq -er ".\"run-70%-timestamp(ns)\""); then
+	if num_load_op=$(hjson-cli -j < info.json | jq -er ".\"num-load-op\""); then
+		num_op=$(($num_total_op - $num_load_op))
+	else
+		num_op=$num_total_op
+	fi
+	num_op=$(($num_op * 3 / 10))
+else
+	num_op=$(($num_total_op * 7 / 10))
+	res=$(awk "
+	{
+		if (NR > 1 && \$2 >= $num_op) {
+			print \$1, \$2;
+			exit 0
+		}
+	}" < progress)
+	timestamp_70p=$(echo $res | cut -sd" " -f1)
+	num_op=$(($num_total_op - $(echo $res | cut -sd" " -f2)))
+fi
+echo "$num_op / (($timestamp_100p - $timestamp_70p) / 1000000000)" | bc -l
