3a97f5740ed24b821750fbd5511df3b47d2e8bac
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-05-01 21:45:24 +0800

    plot/general.py: --scatter-label --scatter-tooltip

--- plot/general.py
+++ plot/general.py
@@ -3,23 +3,23 @@
 import sys
 import getopt
 def print_help():
-    print(sys.argv[0] + ' [-o <outputfile>] [-t line|scatter] [--xlable=] [--ylabel=] [--title] [--xscale] [--scatter-label] [--scatter-color]')
+    print(sys.argv[0] + ' [-o <outputfile>] [-t line|scatter] [--xlable=] [--ylabel=] [--title] [--xscale] [--scatter-label] [--scatter-color] [--scatter-tooltip]')
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'ho:t:i', ['xlabel=', 'ylabel=', 'title=', 'xscale=', 'scatter-label', 'scatter-color'])
+    opts, args = getopt.getopt(sys.argv[1:], 'ho:t:', ['xlabel=', 'ylabel=', 'title=', 'xscale=', 'scatter-label', 'scatter-color', 'scatter-tooltip'])
 except getopt.GetoptError:
     print_help()
     exit(1)
 
 pdf_path = None
 chart_type = 'line'
-interactive = False
 xlabel = None
 ylabel = None
 title = None
 xscale = None
 scatter_label = False
 scatter_color = False
+scatter_tooltip = False
 for opt, arg in opts:
     if opt == '-h':
         print_help()
@@ -28,8 +28,6 @@ for opt, arg in opts:
         pdf_path = arg
     elif opt == '-t':
         chart_type = arg
-    elif opt == '-i':
-        interactive = True
     elif opt == '--xlabel':
         xlabel = arg
     elif opt == '--ylabel':
@@ -42,6 +40,8 @@ for opt, arg in opts:
         scatter_label = True
     elif opt == '--scatter-color':
         scatter_color = True
+    elif opt == '--scatter-tooltip':
+        scatter_tooltip = True
 
 if xlabel is not None:
     assert ylabel is not None
@@ -94,10 +94,17 @@ else:
         'x': [float(v) for v in c[0]],
         'y': [float(v) for v in c[1]],
     }
+    i = 2
     if scatter_label:
-        labels = c[2]
+        labels = c[i]
+        i += 1
     if scatter_color:
-        args['c'] = c[3]
+        args['c'] = c[i]
+        i += 1
+    if scatter_tooltip:
+        tooltips = c[i]
+        i += 1
+    assert i == len(c)
     scatter = ax.scatter(**args)
 ax.set_xlabel(xlabel, fontdict=fonten)
 if ylabel is not None:
@@ -108,13 +115,16 @@ if xscale is not None:
     ax.set_xscale(xscale)
 if legends is not None:
     plt.legend(legends, prop={'size': fontsize})
+if scatter_label:
+    for i in range(0, len(labels)):
+        plt.text(args['x'][i] + 0.01, args['y'][i], labels[i], fontsize=6)
 if pdf_path is not None:
     plt.savefig(pdf_path)
     print('Plot saved to ' + pdf_path)
-if interactive:
+if scatter_tooltip:
     assert chart_type == 'scatter'
     import mpld3
-    tooltip = mpld3.plugins.PointLabelTooltip(scatter, labels=labels)
+    tooltip = mpld3.plugins.PointLabelTooltip(scatter, labels=tooltips)
     mpld3.plugins.connect(fig, tooltip)
     mpld3.show()
 elif 'DISPLAY' in os.environ:
