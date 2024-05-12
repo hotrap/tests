73a349fe69ac58ae1345911a69127b9c163d0e70
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-05-12 06:32:47 +0000

    Load trace and run trace

--- workloads/test-rocksdb-fd-replay-110GB.sh
+++ workloads/test-rocksdb-fd-replay-110GB.sh
@@ -19,6 +19,5 @@ kvexe_dir=$workspace/kvexe-rocksdb/build/
 
 ulimit -n 100000
 cd $DIR
-$kvexe_dir/rocksdb-kvexe --load --format=plain-length-only --num_threads=16 --max_background_jobs=8 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=671088640 --enable_fast_process --db_path=$workspace/testdb/db/ --db_paths="{{$workspace/testdb/fd,1000000000000}}" --trace=$trace_file_load $extra_kvexe_args 2>> log.txt
-$workspace/tests/helper/exe-while.sh . bash -c "$kvexe_dir/rocksdb-kvexe --run --format=plain-length-only --switches=0x1 --num_threads=16 --max_background_jobs=8 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=671088640 --enable_fast_process --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,1000000000000}}\" --trace=$trace_file_run $extra_kvexe_args 2>> log.txt"
+$workspace/tests/helper/exe-while.sh . bash -c "$kvexe_dir/rocksdb-kvexe --load=$trace_file_load --run=$trace_file_run --format=plain-length-only --switches=0x1 --num_threads=16 --max_background_jobs=8 --block_size=16384 --cache_size=75497472 --max_bytes_for_level_base=671088640 --enable_fast_process --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,1000000000000}}\" $extra_kvexe_args 2>> log.txt"
 bash $workspace/tests/helper/rocksdb-data.sh .
