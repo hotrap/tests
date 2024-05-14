759f5b0d648a854d3af01bf0ef730e774fe063c0
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-05-14 06:19:02 +0000

    helper/save-common-data.sh

---
+++ helper/save-common-data.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+if [ ! $2 ]; then
+	echo Usage: $0 db-dir output-dir
+	exit 1
+fi
+db_dir="$1"
+DIR="$2"
+mydir=$(realpath $(dirname $0))
+testdb_dir="$db_dir/.."
+du -sh $testdb_dir/db/ $testdb_dir/fd/ $testdb_dir/sd/ >> "$DIR"/log.txt
+mv $db_dir/{LOG,latency-*,period_stats,progress,cpu,cputimes,mem,info.json,timers,worker-cpu-nanos} "$DIR"/
+if [ -f 0_key_only_trace ]; then
+	find "$db_dir" -name "*_key_only_trace" -exec cat {} \; | awk '{if ($1 == "READ" || $1 == "RMW") print $2}' | $mydir/bin/occurrences > "$DIR"/occurrences
+fi
+"$mydir"/latency-after "$DIR"/
+if [ -f ans_0 ]; then
+	sha256sum "$db_dir"/ans_* > "$DIR"/ans.sha256
+fi
