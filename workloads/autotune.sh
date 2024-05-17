c8b74e9239303cbb9267b625c39a4ad6ade36d21
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-05-17 09:02:33 +0000

    read_0.5_insert_0.5_hotspot0.05_110GB_220GB --load_phase_rate_limit=800000000

--- workloads/autotune.sh
+++ workloads/autotune.sh
@@ -14,11 +14,12 @@ workloads=(
 )
 function run-hotrap {
 	../helper/checkout-$2
-	DIR=../../data/$1/$2-autotune
+	DIR=../../data/$1/$2
 	echo Result directory: $DIR
-	./test-hotrap-110GB.sh ../config/$1 $DIR 5.5GB 330MB "--enable_dynamic_vc_param_in_lsm --enable_dynamic_only_vc_phy_size"
+	./test-hotrap-110GB.sh ../config/$1 $DIR 5.5GB 330MB "--enable_dynamic_vc_param_in_lsm --enable_dynamic_only_vc_phy_size $3"
 	../helper/hotrap-plot.sh $DIR
 }
 for workload in "${workloads[@]}"; do
 	run-hotrap $workload promote-stably-hot
 done
+run-hotrap "read_0.5_insert_0.5_hotspot0.05_110GB_220GB" promote-stably-hot "--load_phase_rate_limit=800000000"
