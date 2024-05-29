d24f94a1973f2d4b0b1cbce3d07bb4b505ea689a
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-05-30 00:41:11 +0800

    tools/last-30p-ops.sh: only run phase

--- tools/last-30p-ops.sh
+++ tools/last-30p-ops.sh
@@ -10,15 +10,31 @@ if timestamp_70p=$(hjson-cli -j < info.json | jq -er ".\"run-70%-timestamp(ns)\"
 	fi
 	num_op=$(($num_op * 3 / 10))
 else
-	num_op=$(($num_total_op * 7 / 10))
-	res=$(awk "
-	{
-		if (NR > 1 && \$2 >= $num_op) {
+	run_start_timestamp=$(hjson-cli -j < info.json | jq -er ".\"run-start-timestamp(ns)\"")
+	run_start_progress=$(awk "
+		BEGIN {
+			res = 0
+		}
+		{
+			if (NR > 1 && \$1 >= $run_start_timestamp) {
+				res = \$2;
+				exit 0
+			}
+		}
+		END {
+			print res
+		}
+	" progress)
+	num_run_op=$(($num_total_op - $run_start_progress))
+	progress_70p=$((run_start_progress + $num_run_op * 7 / 10))
+	res=$(awk "{
+		if (NR > 1 && \$2 >= $progress_70p) {
 			print \$1, \$2;
 			exit 0
 		}
-	}" < progress)
+	}" progress)
 	timestamp_70p=$(echo $res | cut -sd" " -f1)
-	num_op=$(($num_total_op - $(echo $res | cut -sd" " -f2)))
+	progress_70p=$(echo $res | cut -sd" " -f2)
+	num_op=$(($num_total_op - $progress_70p))
 fi
 echo "$num_op / (($timestamp_100p - $timestamp_70p) / 1000000000)" | bc -l
