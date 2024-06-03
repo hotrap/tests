93abbd27932f57e772937d83bfc5533acaa62584
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-06-03 20:16:23 +0800

    Update

--- workloads/test-rocksdb-fd-11GB.sh
+++ workloads/test-rocksdb-fd-11GB.sh
@@ -3,25 +3,4 @@ if [[ $# < 2 || $# > 3 ]]; then
 	echo Usage: $0 workload-file output-dir [extra-kvexe-args]
 	exit 1
 fi
-set -e
-set -o pipefail
-# To make set -e take effect if the output dir does not exists
-mkdir -p $2
-res="$(ls -A $2)"
-if [ "$res" ]; then
-	echo "$2" is not empty!
-	exit 1
-fi
-workload_file=$(realpath $1)
-DIR=$(realpath "$2")
-extra_kvexe_args="$3"
-cd "$(dirname $0)"
-workspace=$(realpath ../..)
-kvexe_dir=$workspace/kvexe-rocksdb/build/
-
-ulimit -n 100000
-# Dump core when crash
-ulimit -c unlimited
-cd $DIR
-$workspace/tests/helper/exe-while.sh . bash -c "LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 $kvexe_dir/rocksdb-kvexe --switches=0x1 --num_threads=16 --max_background_jobs=4 --block_size=16384 --cache_size=134217728 --max_bytes_for_level_base=67108864 --level0_file_num_compaction_trigger=1 --enable_fast_generator --enable_fast_process --workload_file=$workload_file --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,100000000000}}\" $extra_kvexe_args 2>> log.txt"
-bash $workspace/tests/helper/rocksdb-data.sh .
+$(dirname $0)/test-rocksdb-fd-11GB-generic.sh "$2" "--workload_file=$1 --switches=0x1 $3"
--- workloads/test-rocksdb-fd-correctness-11GB.sh
+++ workloads/test-rocksdb-fd-correctness-11GB.sh
@@ -1,31 +1,7 @@
 #!/usr/bin/env bash
 if [[ $# < 2 || $# > 3 ]]; then
-	echo Usage: $0 workload output-dir [switches]
+	echo Usage: $0 workload output-dir [extra-kvexe-args]
 	exit 1
 fi
-set -e
-set -o pipefail
-# To make set -e take effect if the output dir does not exists
-mkdir -p $2
-res="$(ls -A $2)"
-if [ "$res" ]; then
-	echo "$2" is not empty!
-	exit 1
-fi
-workload="$1"
-DIR=$(realpath "$2")
-if [ $3 ]; then
-	switches=$3
-else
-	switches=0x3
-fi
-cd "$(dirname $0)"
 workspace=$(realpath ../..)
-kvexe_dir=$workspace/kvexe-rocksdb/build/
-
-ulimit -n 100000
-# Dump core when crash
-ulimit -c unlimited
-cd $DIR
-$workspace/tests/helper/exe-while.sh . bash -c "LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 $kvexe_dir/rocksdb-kvexe --load=$workspace/YCSB-traces/$workload-load --run=$workspace/YCSB-traces/$workload-run --format=plain-length-only --switches=$switches --num_threads=16 --max_background_jobs=8 --block_size=16384 --max_bytes_for_level_base=67108864 --level0_file_num_compaction_trigger=1 --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,100000000000}}\" 2>> log.txt"
-bash $workspace/tests/helper/rocksdb-data.sh .
+$(dirname $0)/test-rocksdb-fd-11GB-generic.sh "$2" "--load=$workspace/YCSB-traces/$1-load --run=$workspace/YCSB-traces/$1-run --format=plain-length-only --switches=0x3 $3"
