e06677dd31c9990eb30eb25c6d12514ac065367d
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-06-25 17:42:52 +0800

    Check correctness of hotrap under 110GB workloads

--- workloads/test-hotrap-11GB.sh
+++ workloads/test-hotrap-11GB.sh
@@ -3,4 +3,5 @@ if [[ $# < 2 || $# > 3 ]]; then
 	echo Usage: $0 workload-file output-dir [extra-kvexe-args]
 	exit 1
 fi
-$(dirname $0)/test-hotrap-11GB-generic.sh "$2" "--workload_file=$1 --switches=0x1 $3"
+workload_file=$(realpath -s "$1")
+$(dirname $0)/test-hotrap-11GB-generic.sh "$2" "--enable_fast_generator --workload_file=$workload_file --switches=0x1 $3"
