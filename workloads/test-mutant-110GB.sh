929b9668164d0bbb38cd620a18fa0e137114783a
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-07-03 05:47:37 +0000

    Update

--- workloads/test-mutant-110GB.sh
+++ workloads/test-mutant-110GB.sh
@@ -18,5 +18,6 @@ workspace=$(realpath ../..)
 
 ulimit -n 100000
 ../helper/exe-while.sh $DIR bash -c "systemd-run --user --scope -p MemoryMax=4G nocache $workspace/kvexe-mutant/build/rocksdb-kvexe --cleanup --format=ycsb --num_threads=16 --switches=1 --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,$fd_size},{$workspace/testdb/sd,$sd_size}}\" --costs=\"{0.528, 0.045}\" --target_cost=0.4 --enable_fast_process --enable_fast_generator --workload_file=$workload_file >/dev/null 2> $DIR/log.txt"
-cp $workspace/testdb/db/{a*,b*,v*,d*,e*,f*,h*,g*,i*,j*,k*,l*,o*,p*,q*,r*,s*,t*,u*,v*,w*,x*,y*,z*,n*,m*,LOG,log.txt,cpu,first-level-in-sd,progress,promoted-*,rocksdb-stats.txt,period_stats,latency*,mem} $DIR/
+mv $workspace/testdb/db/latency* $DIR/
+cp $workspace/testdb/db/{a*,b*,v*,d*,e*,f*,h*,g*,i*,j*,k*,l*,o*,p*,q*,r*,s*,t*,u*,v*,w*,x*,y*,z*,n*,m*,LOG,log.txt,cpu,first-level-in-sd,progress,promoted-*,rocksdb-stats.txt,period_stats,mem} $DIR/
 ../helper/last-10p-latency.py $DIR
