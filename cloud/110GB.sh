a635e76ad340a212acc2c2f9a11e6358c81158a1
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-07-03 05:46:29 +0000

    Update cloud/110GB.sh

--- cloud/110GB.sh
+++ cloud/110GB.sh
@@ -7,22 +7,7 @@ output_dir=$(realpath $2)
 user=$(cat $config_file | jq -er ".user")
 cd $(dirname $0)
 
-workloads=(
-	"read_0.75_insert_0.25_hotspot0.05_110GB_220GB"
-	"ycsba_hotspot0.05_110GB_220GB"
-	"ycsbc_hotspot0.05_110GB_220GB"
-	"read_0.5_insert_0.5_zipfian_110GB_220GB"
-	"read_0.75_insert_0.25_zipfian_110GB_220GB"
-	"ycsba_zipfian_110GB_220GB"
-	"ycsbc_zipfian_110GB_220GB"
-	"read_0.5_insert_0.5_uniform_110GB_220GB"
-	"read_0.75_insert_0.25_uniform_110GB_220GB"
-	"ycsba_uniform_110GB_220GB"
-	"ycsbc_uniform_110GB_220GB"
-)
-
 source common.sh
-check-workload-files "${workloads[@]}"
 
 function run-rocksdb-fd {
 	workload=$1
@@ -60,6 +45,32 @@ function run-hotrap {
 	rsync -zrpt --partial -e ssh $user@$IP:~/data/$workload $output_dir/
 	../helper/hotrap-plot.sh $output_dir/$workload/$version
 }
+function run-u135542 {
+	workload="u135542"
+	version=$2
+	IP=$3
+	./checkout-hotrap $user $IP $version
+	ssh $user@$IP -o ServerAliveInterval=60 "source ~/.profile && cd tests/workloads && ./test-hotrap-110GB-generic.sh ../../data/$workload/$version \"LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4\" \"--enable_fast_generator --workload=$workload --switches=0x1 ${@:4}\""
+	rsync -zrpt --partial -e ssh $user@$IP:~/data/$workload $output_dir/
+	../helper/hotrap-plot.sh $output_dir/$workload/$version
+}
+
+workloads=(
+	"read_0.75_insert_0.25_hotspot0.05_110GB_220GB"
+	"ycsba_hotspot0.05_110GB_220GB"
+	"ycsbc_hotspot0.05_110GB_220GB"
+	"read_0.5_insert_0.5_zipfian_110GB_220GB"
+	"read_0.75_insert_0.25_zipfian_110GB_220GB"
+	"ycsba_zipfian_110GB_220GB"
+	"ycsbc_zipfian_110GB_220GB"
+	"read_0.5_insert_0.5_uniform_110GB_220GB"
+	"read_0.75_insert_0.25_uniform_110GB_220GB"
+	"ycsba_uniform_110GB_220GB"
+	"ycsbc_uniform_110GB_220GB"
+)
+check-workload-files "${workloads[@]}"
+
+cloud-run run-u135542 "u135542" promote-stably-hot
 
 workload="read_0.5_insert_0.5_hotspot0.05_110GB_220GB"
 cloud-run run-rocksdb-fd $workload rocksdb-fd
@@ -74,9 +85,31 @@ for workload in "${workloads[@]}"; do
 	cloud-run run-hotrap $workload promote-stably-hot
 done
 cloud-run run-hotrap "ycsbc_hotspot0.01_110GB_220GB" promote-stably-hot
-cloud-run run-hotrap "ycsbc_hotspotshifting0.05_110GB_220GB" promote-stably-hot
-cloud-run run-hotrap "ycsbc_hotspotshifting0.05_0.02_110GB_220GB" promote-stably-hot
 cloud-run run-hotrap "ycsbc_uniform_110GB_220GB" promote-accessed
 cloud-run run-hotrap "read_0.75_insert_0.25_hotspot0.05_110GB_220GB" no-retain
 cloud-run run-hotrap "read_0.75_insert_0.25_hotspot0.05_110GB_220GB" no-promote-by-compaction
+
+hotspot_workloads=(
+	"read_0.5_insert_0.5_hotspot0.05_110GB_220GB_200B"
+	"read_0.75_insert_0.25_hotspot0.05_110GB_220GB_200B"
+	"ycsba_hotspot0.05_110GB_220GB_200B"
+	"ycsbc_hotspot0.05_110GB_220GB_200B"
+)
+uniform_workloads=(
+	"read_0.5_insert_0.5_uniform_110GB_220GB_200B"
+	"read_0.75_insert_0.25_uniform_110GB_220GB_200B"
+	"ycsba_uniform_110GB_220GB_200B"
+	"ycsbc_uniform_110GB_220GB_200B"
+)
+check-workload-files "${hotspot_workloads[@]}"
+check-workload-files "${uniform_workloads[@]}"
+
+for workload in "${hotspot_workloads[@]}"; do
+	cloud-run run-hotrap $workload promote-stably-hot
+	cloud-run run-rocksdb-fd $workload rocksdb-fd
+done
+for workload in "${uniform_workloads[@]}"; do
+	cloud-run run-hotrap $workload promote-stably-hot
+	cloud-run run-rocksdb $workload rocksdb-fat
+done
 wait
