4aa9909e20b71db5e7eee85a36cc94c67f025f42
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-07-07 15:00:16 +0800

    Don't export ans when replay

--- workloads/test-rocksdb-fd-110GB-replay.sh
+++ workloads/test-rocksdb-fd-110GB-replay.sh
@@ -5,4 +5,4 @@ if [[ $# < 3 || $# > 4 ]]; then
 fi
 trace_file_load=$(realpath $1)
 trace_file_run=$(realpath $2)
-$(dirname $0)/test-rocksdb-fd-110GB-generic.sh "$3" "LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4" "--load=$trace_file_load --run=$trace_file_run --format=plain-length-only --switches=0x3 $4"
+$(dirname $0)/test-rocksdb-fd-110GB-generic.sh "$3" "LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4" "--load=$trace_file_load --run=$trace_file_run --format=plain-length-only --switches=0x1 $4"
