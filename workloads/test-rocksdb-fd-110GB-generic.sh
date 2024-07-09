6a52dc34d5792b67ebac2093cc6fa15d6a43aa59
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-07-09 02:22:17 +0000

    rocksdb-fd use default level base size

--- workloads/test-rocksdb-fd-110GB-generic.sh
+++ workloads/test-rocksdb-fd-110GB-generic.sh
@@ -4,12 +4,11 @@ if [[ $# < 1 || $# > 3 ]]; then
 	exit 1
 fi
 mkdir -p $1
-res="$(ls -A $1)"
-if [ "$res" ]; then
-	echo "$1" is not empty!
+DIR=$(realpath "$1")
+if [ "$(ls -A $DIR)" ]; then
+    echo "$1" is not empty!
     exit 1
 fi
-DIR=$(realpath "$1")
 prefix="$2"
 extra_kvexe_args="$3"
 cd "$(dirname $0)"
@@ -20,5 +19,5 @@ ulimit -n 100000
 # Dump core when crash
 ulimit -c unlimited
 cd $DIR
-$workspace/tests/helper/exe-while.sh . bash -c "$prefix $kvexe_dir/rocksdb-kvexe --num_threads=16 --max_background_jobs=8 --block_size=16384 --cache_size=134217728 --max_bytes_for_level_base=671088640 --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,1000000000000}}\" $extra_kvexe_args 2>> log.txt"
+$workspace/tests/helper/exe-while.sh . bash -c "$prefix $kvexe_dir/rocksdb-kvexe --num_threads=16 --max_background_jobs=8 --block_size=16384 --cache_size=134217728 --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,1000000000000}}\" $extra_kvexe_args 2>> log.txt"
 bash $workspace/tests/helper/rocksdb-data.sh .
