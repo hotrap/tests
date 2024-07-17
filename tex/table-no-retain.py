1fa1a2f1b944d780195350fa7061e5035d420137
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-07-17 03:04:27 +0000

    table-no-retain.py: Compaction I/O

--- tex/table-no-retain.py
+++ tex/table-no-retain.py
@@ -26,18 +26,23 @@ def calc(data_dir):
     promoted = num_bytes['2fdlast'] + num_bytes['by-flush']
     retained = num_bytes['retained']
 
+    compaction_bytes = run_phase(version_data.info(), common.read_compaction_bytes(data_dir))
+    compaction_bytes['read'] -= compaction_bytes.iloc[0]['read']
+    compaction_bytes['write'] -= compaction_bytes.iloc[0]['write']
+    compaction_io = compaction_bytes.iloc[-1]['read'] + compaction_bytes.iloc[-1]['write']
+
     hit_rates = common.read_hit_rates(data_dir)
     final_hit_rate = hit_rates['hit-rate'][int(len(hit_rates) * 0.99):].mean()
 
-    return (promoted, retained, final_hit_rate)
+    return (promoted, retained, compaction_io, final_hit_rate)
 
-(hotrap_promoted, hotrap_retained, hotrap_hit_rate) = calc(os.path.join(dir, 'promote-stably-hot'))
-(promoted, retained, hit_rate) = calc(os.path.join(dir, 'no-retain'))
+(hotrap_promoted, hotrap_retained, hotrap_compaction_io, hotrap_hit_rate) = calc(os.path.join(dir, 'promote-stably-hot'))
+(promoted, retained, compaction_io, hit_rate) = calc(os.path.join(dir, 'no-retain'))
 
 tex = io.StringIO()
-print('\\begin{tabular}{|c|c|c|c|}\n\t\\hline\n\tVersion & Promoted & Retained & Final hit rate \\\\\n\t\hline', file=tex)
-print('\tHotRAP & %.1fGB & %.1fGB & %.1f\\%% \\\\\n\t\\hline' %(hotrap_promoted / 1e9, hotrap_retained / 1e9, hotrap_hit_rate * 100), file=tex)
-print('\tno-retain & %.1fGB & %.1fGB & %.1f\\%% \\\\\n\t\hline' %(promoted / 1e9, retained / 1e9, hit_rate * 100), file=tex)
+print('\\begin{tabular}{|c|c|c|c|c|}\n\t\\hline\n\tVersion & Promoted & Retained & Compaction & Hit rate \\\\\n\t\hline', file=tex)
+print('\tHotRAP & %.1fGB & %.1fGB & %.1fGB & %.1f\\%% \\\\\n\t\\hline' %(hotrap_promoted / 1e9, hotrap_retained / 1e9, hotrap_compaction_io / 1e9, hotrap_hit_rate * 100), file=tex)
+print('\tno-retain & %.1fGB & %.1fGB & %.1fGB & %.1f\\%% \\\\\n\t\hline' %(promoted / 1e9, retained / 1e9, compaction_io / 1e9, hit_rate * 100), file=tex)
 print('\\end{tabular}', file=tex)
 tex = tex.getvalue()
 print(tex)
