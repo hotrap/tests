122cbc424cf37dfc660d55d0a83ae11751ca09f9
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-08-05 05:47:01 +0000

    last 10p hit rate and ops

--- tools/last-10p-hit-rate.py
+++ tools/last-10p-hit-rate.py
@@ -5,10 +5,7 @@ import os
 sys.path.insert(0, os.path.join(os.path.dirname(sys.argv[0]), '../helper/'))
 import common
 
-import json5
-info = json5.load(open('info.json'))
-run_70p_ts = info['run-70%-timestamp(ns)']
-
+d = common.VersionData('.')
 hit_rates = common.read_hit_rates('.')
-hit_rates = hit_rates[hit_rates['Timestamp(ns)'] > run_70p_ts]
+hit_rates = hit_rates[hit_rates['Timestamp(ns)'] > d.ts_run_90p()]
 print(hit_rates['hit-rate'].mean())
