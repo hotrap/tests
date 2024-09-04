88917b05d510da9489c9dd346c0df89390375cbc
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-09-04 15:10:30 +0000

    --enable_auto_tuning

--- workloads/test-hotrap-11GB-generic.sh
+++ workloads/test-hotrap-11GB-generic.sh
@@ -23,5 +23,5 @@ ulimit -n 100000
 # Dump core when crash
 ulimit -c unlimited
 cd $DIR
-$workspace/tests/helper/exe-while.sh . sh -c "LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 $kvexe_dir/rocksdb-kvexe --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --max_viscnts_size=$max_viscnts_size --num_threads=16 --block_size=16384 --max_bytes_for_level_base=67108864 --level0_file_num_compaction_trigger=1 --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,$fd_size},{$workspace/testdb/sd,100000000000}}\" --viscnts_path=$workspace/testdb/viscnts $extra_kvexe_args 2>> log.txt"
+$workspace/tests/helper/exe-while.sh . sh -c "LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 $kvexe_dir/rocksdb-kvexe --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --max_viscnts_size=$max_viscnts_size --num_threads=16 --block_size=16384 --max_bytes_for_level_base=67108864 --level0_file_num_compaction_trigger=1 --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,$fd_size},{$workspace/testdb/sd,100000000000}}\" --viscnts_path=$workspace/testdb/viscnts --enable_auto_tuning $extra_kvexe_args 2>> log.txt"
 $workspace/tests/helper/hotrap-data.sh .
