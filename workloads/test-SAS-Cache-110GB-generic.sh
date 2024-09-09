6da0ba29f016614fc3a89b640bcdf9f11a199c49
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-09-09 04:51:30 +0000

    SAS-Cache 6GB

--- workloads/test-SAS-Cache-110GB-generic.sh
+++ workloads/test-SAS-Cache-110GB-generic.sh
@@ -13,12 +13,12 @@ prefix="$2"
 extra_kvexe_args="$3"
 workspace=$(realpath $(dirname $0)/../..)
 kvexe_dir=$workspace/kvexe-SAS-Cache/build/
-fd_size=4880000000
+fd_size=4000000000
 
 memtable_size=$((64 * 1024 * 1024))
 L1_size=$(($fd_size / 12 / $memtable_size * $memtable_size))
 
 ulimit -n 100000
 cd $DIR
-$workspace/tests/helper/exe-while.sh . sh -c "$prefix $kvexe_dir/rocksdb-kvexe --switches=0x1 --num_threads=16 --block_size=16384 --cache_size=134217728 --max_bytes_for_level_base=$L1_size --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,$fd_size},{$workspace/testdb/sd,1000000000000}}\" $extra_kvexe_args 2>> log.txt"
+$workspace/tests/helper/exe-while.sh . sh -c "$prefix $kvexe_dir/rocksdb-kvexe --switches=0x1 --num_threads=16 --block_size=16384 --cache_size=134217728 --secondary_cache_size_MiB=6000 --max_bytes_for_level_base=$L1_size --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,$fd_size},{$workspace/testdb/sd,1000000000000}}\" $extra_kvexe_args 2>> log.txt"
 $workspace/tests/helper/rocksdb-data.sh .
