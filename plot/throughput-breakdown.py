3c2965f51412c8c51f6b5a76b15d7388374aa8da
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-09-14 05:20:36 +0000

    Clean up

--- plot/throughput-breakdown.py
+++ plot/throughput-breakdown.py
@@ -10,12 +10,14 @@ mean_step = int(sys.argv[2])
 
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(sys.argv[0]), '../helper/'))
-sys.path.insert(0, os.path.join(os.path.dirname(sys.argv[0]), 'common/'))
+import common
 
+import json5
+import numpy as np
+import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-
-import throughput_breakdown
+import matplotlib.gridspec as gridspec
 
 # Paper specific settings
 DOUBLE_COL_WIDTH = 17.8
@@ -29,7 +31,7 @@ mpl.rcParams.update({
 })
 plt.rcParams['axes.unicode_minus'] = False
 
-fig = plt.figure(dpi = 300, figsize = (cm_to_inch(DOUBLE_COL_WIDTH), cm_to_inch(3.5)), constrained_layout=True)
+figure = plt.figure(dpi = 300, figsize = (cm_to_inch(DOUBLE_COL_WIDTH), cm_to_inch(3.5)), constrained_layout=True)
 
 versions=[
     {
@@ -45,9 +47,73 @@ versions=[
         'path': 'promote-stably-hot',
     },
 ]
-throughput_breakdown.draw_throughput_breakdown(fig, dir, versions, mean_step, linewidth=0.5, num_marks=5, markersize=1, markersize_x=2)
 
-fig.legend(['FD', 'SD', 'FD-Compaction', 'SD-Compaction', 'Get'], fontsize=8, ncol=5, loc='center', bbox_to_anchor=(0.5, 1.08))
+linewidth=0.5
+num_marks=5
+markersize=1
+markersize_x=2
+
+gs = gridspec.GridSpec(1, len(versions), figure=figure)
+for (i, version) in enumerate(versions):
+    subfig = plt.subplot(gs[0, i])
+    ax = plt.gca()
+    ax.set_axisbelow(True)
+    ax.grid(axis='y')
+    data_dir = os.path.join(dir, version['path'])
+
+    first_level_in_sd = int(open(data_dir + '/first-level-in-sd').read())
+    info = os.path.join(data_dir, 'info.json')
+    info = json5.load(open(info))
+    def read_table(file):
+        iostat_raw = pd.read_table(file, sep='\s+')
+        iostat_raw = iostat_raw[(iostat_raw['Timestamp(ns)'] >= info['run-start-timestamp(ns)']) & (iostat_raw['Timestamp(ns)'] < info['run-end-timestamp(ns)'])]
+        iostat_raw['Time(Seconds)'] = (iostat_raw['Timestamp(ns)'] - iostat_raw['Timestamp(ns)'].iloc[0]) / 1e9
+        iostat = iostat_raw[['rkB/s', 'wkB/s']].groupby(iostat_raw.index // mean_step).mean()
+        iostat['Time(Seconds)'] = iostat_raw['Time(Seconds)'].groupby(iostat_raw.index // mean_step).first()
+        return iostat
+    fd = read_table(data_dir + '/iostat-fd.txt')
+    sd = read_table(data_dir + '/iostat-sd.txt')
+
+    compaction_bytes = common.read_compaction_bytes_fd_sd(data_dir, first_level_in_sd)
+    timestamps = np.array(compaction_bytes['Timestamp(ns)'])
+    run_phase = (timestamps >= info['run-start-timestamp(ns)']) & (timestamps < info['run-end-timestamp(ns)'])
+    timestamps = timestamps[run_phase]
+    compaction_bytes = compaction_bytes[run_phase]
+    time = (timestamps[1:] - info['run-start-timestamp(ns)']) / 1e9
+
+    throughput = compaction_bytes[1:].values - compaction_bytes[:-1].values
+    throughput = throughput[:,1:] / (throughput[:,0][:, np.newaxis] / 1e9)
+    throughput = pd.DataFrame(throughput, columns=['fd-read', 'fd-write', 'sd-read', 'sd-write'])
+    throughput['Time(Seconds)'] = time
+    throughput = throughput.groupby(throughput.index // mean_step).mean()
+
+    markevery = int(len(fd['Time(Seconds)']) / num_marks)
+    ax.plot(sd['Time(Seconds)'], (fd['rkB/s'] + fd['wkB/s']) / 1e3, marker='o', linewidth=linewidth, markersize=markersize, markevery=markevery)
+    ax.plot(sd['Time(Seconds)'], (sd['rkB/s'] + sd['wkB/s']) / 1e3, marker='D', linewidth=linewidth, markersize=markersize, markevery=markevery)
+    markevery = int(len(throughput['Time(Seconds)']) / num_marks)
+    ax.plot(throughput['Time(Seconds)'], (throughput['fd-read'] + throughput['fd-write']) / 1e6, marker='s', linewidth=linewidth, markersize=markersize, markevery=markevery)
+    ax.plot(throughput['Time(Seconds)'], (throughput['sd-read'] + throughput['sd-write']) / 1e6, marker='x', linewidth=linewidth, markersize=markersize_x, markevery=markevery)
+
+    rand_read_bytes = common.read_rand_read_bytes_fd_sd(data_dir, first_level_in_sd)
+    rand_read_bytes = rand_read_bytes[(info['run-start-timestamp(ns)'] <= rand_read_bytes['Timestamp(ns)']) & (rand_read_bytes['Timestamp(ns)'] < info['run-end-timestamp(ns)'])]
+    time = (rand_read_bytes['Timestamp(ns)'][1:] - info['run-start-timestamp(ns)']) / 1e9
+    rand_read_bytes = rand_read_bytes['fd'] + rand_read_bytes['sd']
+    throughput = rand_read_bytes[1:].values - rand_read_bytes[:-1].values
+    get_throughput = pd.DataFrame({
+        'Time(s)': time,
+        'Throughput(B/s)': throughput,
+    })
+    get_throughput = get_throughput.groupby(get_throughput.index // mean_step).mean()
+    ax.plot(get_throughput['Time(s)'], get_throughput['Throughput(B/s)'] / 1e6, color='black', linestyle='dashed', linewidth=linewidth, markersize=markersize, markevery=markevery)
+    subfig.text(0.5, -0.30, 'Time (Seconds)', fontsize=8, ha='center', va='center', transform=subfig.transAxes)
+    plt.xticks(fontsize=8)
+    plt.yticks(fontsize=8)
+    ax.set_ylim(bottom=0)
+    plt.xlabel(version['name'], labelpad=10, fontsize=8)
+    if i == 0:
+        plt.ylabel('Throughput (MB/s)', fontsize=8)
+
+figure.legend(['FD', 'SD', 'FD-Compaction', 'SD-Compaction', 'Get'], fontsize=8, ncol=5, loc='center', bbox_to_anchor=(0.5, 1.08))
 pdf_path = 'throughput-breakdown.pdf'
 plt.savefig(pdf_path, bbox_inches='tight', pad_inches=0.01)
 print('Plot saved to ' + pdf_path)
