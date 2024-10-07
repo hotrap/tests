49d402d17f94a114fc1fa26cdaa1c461b71d1a0a
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-10-07 17:45:51 +0800

    workloads/11GB.sh

---
+++ workloads/11GB.sh
@@ -0,0 +1,50 @@
+workloads=(
+	"read_0.5_insert_0.5_hotspot0.05_11GB"
+	"ycsba_hotspot0.05_11GB"
+	"ycsbc_hotspot0.05_11GB"
+	"read_0.5_insert_0.5_uniform_11GB"
+	"ycsba_uniform_11GB"
+	"ycsbc_uniform_11GB"
+	"read_0.5_insert_0.5_zipfian_11GB"
+	"ycsba_zipfian_11GB"
+	"ycsbc_zipfian_11GB"
+)
+function run-rocksdb-fd {
+	../helper/checkout-rocksdb
+	DIR=../../data/$1/rocksdb-fd
+	echo Result directory: $DIR
+	./test-rocksdb-fd-11GB.sh ../config/$1 $DIR
+	../helper/rocksdb-plot-11GB.sh $DIR
+}
+function run-rocksdb {
+	../helper/checkout-$2
+	DIR=../../data/$1/$2
+	echo Result directory: $DIR
+	./test-rocksdb-11GB.sh ../config/$1 $DIR 1GB
+	../helper/rocksdb-plot-11GB.sh $DIR
+}
+function run-secondary-cache {
+	../helper/checkout-$2
+	DIR=../../data/$1/$2
+	echo Result directory: $DIR
+	./test-secondary-cache-11GB.sh ../config/$1 $DIR
+	../helper/rocksdb-plot-11GB.sh $DIR
+}
+function run-hotrap {
+	../helper/checkout-$2
+	DIR=../../data/$1/$2
+	echo Result directory: $DIR
+	# Reserve 33MB for VisCnts
+	./test-hotrap-11GB.sh ../config/$1 $DIR
+	../helper/hotrap-plot-11GB.sh $DIR
+}
+for workload in "${workloads[@]}"; do
+	run-rocksdb-fd $workload
+	run-secondary-cache $workload secondary-cache
+	run-rocksdb $workload rocksdb-fat
+	run-hotrap $workload promote-stably-hot
+done
+run-hotrap "read_0.75_insert_0.25_hotspot0.05_11GB" promote-stably-hot
+run-hotrap "ycsbc_uniform_11GB" promote-accessed
+run-hotrap "read_0.75_insert_0.25_hotspot0.05_11GB" no-retain
+run-hotrap "read_0.75_insert_0.25_hotspot0.05_11GB" no-promote-by-compaction
---
+++ workloads/test-hotrap-11GB-generic.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+if [ $# -lt 1 -o $# -gt 3 ]; then
+	echo Usage: $0 output-dir [prefix] [extra-kvexe-args]
+	exit 1
+fi
+mkdir -p $1
+DIR=$(realpath "$1")
+if [ "$(ls -A $DIR)" ]; then
+	echo "$1" is not empty!
+	exit 1
+fi
+prefix="$2"
+extra_kvexe_args="$3"
+cd "$(dirname $0)"
+workspace=$(realpath ../..)
+kvexe_dir=$workspace/kvexe/build/
+
+fd_size=1000000000
+max_hot_set_size=500000000
+max_viscnts_size=33000000
+
+ulimit -n 100000
+# Dump core when crash
+ulimit -c unlimited
+cd $DIR
+$workspace/tests/helper/exe-while.sh . sh -c "$prefix $kvexe_dir/rocksdb-kvexe --compaction_pri=5 --max_hot_set_size=$max_hot_set_size --max_viscnts_size=$max_viscnts_size --num_threads=16 --max_bytes_for_level_base=67108864 --level0_file_num_compaction_trigger=1 --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,$fd_size},{$workspace/testdb/sd,100000000000}}\" --viscnts_path=$workspace/testdb/viscnts --enable_auto_tuning $extra_kvexe_args 2>> log.txt"
+$workspace/tests/helper/hotrap-data.sh .
