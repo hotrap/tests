e1143f58c5da1529f5f58d8e36d862bb3f12cbb7
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-10-14 07:15:45 +0000

    workloads/dbbench.sh: run-rocksdb

--- workloads/dbbench.sh
+++ workloads/dbbench.sh
@@ -1,4 +1,14 @@
-../helper/checkout-hotrap
-DIR=../../data/mixgraph/hotrap
-./dbbench-hotrap.sh "$DIR"
-../helper/dbbench-hotrap-plot.sh "$DIR"
+function run-rocksdb {
+	../helper/checkout-rocksdb-tiered
+	DIR=../../data/mixgraph/rocksdb-tiered
+	./dbbench-rocksdb-tiered.sh "$DIR"
+	../helper/dbbench-rocksdb-plot.sh "$DIR"
+}
+function run-hotrap {
+	../helper/checkout-hotrap
+	DIR=../../data/mixgraph/hotrap
+	./dbbench-hotrap.sh "$DIR"
+	../helper/dbbench-hotrap-plot.sh "$DIR"
+}
+run-rocksdb
+run-hotrap
