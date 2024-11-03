9e520258bad39b0a74088050ea16b420b6afb8d8
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-11-03 06:57:58 +0000

    More compatible tools/report-sum.py

--- tools/report-sum.py
+++ tools/report-sum.py
@@ -3,13 +3,18 @@
 import pandas as pd
 
 fields = [
+    'rocksdb.t0.hit',
+    'rocksdb.t1.hit',
     'hotrap.promoted.flush.bytes',
     'hotrap.promoted.2fdlast.bytes',
     'hotrap.promoted.2sdfront.bytes',
     'hotrap.retained.bytes',
     'hotrap.accessed.cold.bytes',
     'hotrap.has_newer_version.bytes',
+    'hotrap.scan.hit.l0',
+    'hotrap.scan.hit.l1',
 ]
 report = pd.read_table('report.csv', sep=',')
 for field in fields:
-    print(field, report[field].sum())
+    if field in report:
+        print(field, report[field].sum())
