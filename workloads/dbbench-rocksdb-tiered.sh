cafdb6f95c5873a73d5730f75c5b60ac4b93208c
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-11-03 15:27:31 +0800

    rocksdb-tiered: --mix_seek_ratio=0.03

--- workloads/dbbench-rocksdb-tiered.sh
+++ workloads/dbbench-rocksdb-tiered.sh
@@ -65,9 +65,9 @@ $workspace/tests/helper/exe-while.sh . $db_bench \
 	--value_sigma=25.45 \
 	--iter_k=2.517 \
 	--iter_sigma=14.236 \
-	--mix_get_ratio=0.85 \
+	--mix_get_ratio=0.83 \
 	--mix_put_ratio=0.14 \
-	--mix_seek_ratio=0.01 \
+	--mix_seek_ratio=0.03 \
 	--sine_mix_rate_interval_milliseconds=5000 \
 	--sine_mix_rate=true \
 	--sine_a=1000 \
