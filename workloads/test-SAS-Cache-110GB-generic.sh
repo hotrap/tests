4912e12bf5b9582e8b3f0262b9a5110fab999935
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2024-12-31 05:40:26 +0000

    SAS-Cache use the whole FD

--- workloads/test-SAS-Cache-110GB-generic.sh
+++ workloads/test-SAS-Cache-110GB-generic.sh
@@ -3,22 +3,6 @@ if [ $# -lt 1 -o $# -gt 3 ]; then
 	echo Usage: $0 output-dir [prefix] [extra-kvexe-args]
 	exit 1
 fi
-mkdir -p $1
-DIR=$(realpath "$1")
-if [ "$(ls -A $DIR)" ]; then
-        echo "$1" is not empty!
-        exit 1
-fi
-prefix="$2"
-extra_kvexe_args="$3"
 workspace=$(realpath $(dirname $0)/../..)
-kvexe_dir=$workspace/kvexe-rocksdb/build/
-fd_size=4000000000
-
-memtable_size=$((64 * 1024 * 1024))
-L1_size=$(($fd_size / 12 / $memtable_size * $memtable_size))
-
-ulimit -n 100000
-cd $DIR
-$workspace/tests/helper/exe-while.sh . sh -c "$prefix $kvexe_dir/rocksdb-kvexe --num_threads=16 --cache_size=335544320 --secondary_cache_size_MiB=6000 --max_bytes_for_level_base=$L1_size --db_path=$workspace/testdb/db/ --db_paths=\"{{$workspace/testdb/fd,$fd_size},{$workspace/testdb/sd,1000000000000}}\" $extra_kvexe_args 2>> log.txt"
-$workspace/tests/helper/rocksdb-data.sh .
+$(dirname $0)/kvexe-generic.sh "$2 $workspace/kvexe-rocksdb/build/rocksdb-kvexe" "$workspace/testdb/db" 320MiB "$1" "--max_bytes_for_level_base=$((12 * 64 * 1024 * 1024)) --db_paths=\"{{$workspace/testdb/sd,10000000000000}}\" --secondary_cache_size_MiB=10000 $3"
+$workspace/tests/helper/rocksdb-data.sh "$1"
