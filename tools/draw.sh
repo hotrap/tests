4865724214d344dcc7c5048b9f3143f4ed90ca5b
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-01-11 05:45:02 +0000

    tools: draw.sh, save.sh

---
+++ tools/draw.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+tests=$(realpath $(dirname $0)/..)
+
+if [ -d ycsbc_zipfian_110GB_220GB ]; then
+	$tests/plot/ycsb-sweep.py .
+	$tests/plot/ops-200B.py .
+	$tests/tex/overhead-uniform-rocksdb-tiered.py .
+	$tests/plot/latency.py .
+
+	$tests/plot/cputime-breakdown-200B.py .
+	$tests/plot/io-breakdown-200B.py .
+
+	cd read_0.75_insert_0.25_hotspot0.05_110GB_220GB
+	$tests/plot/throughput-breakdown.py . 200
+	$tests/tex/table-no-hotness-aware-compaction.py .
+	cd ..
+
+	cd ycsbc_uniform_110GB_220GB
+	$tests/tex/table-promote-accessed.py .
+	cd ..
+
+	$tests/plot/ops-1.1TB.py .
+fi
+
+if [ -d u24685531/hotrap ]; then
+	cd u24685531/hotrap
+	$tests/plot/dymanic-workload.py .
+	cd ../..
+fi
+
+if [ -d cluster02-283x ]; then
+	~/tests/plot/twitter_speedup.py . ../stats
+fi
+
+if [ -d cluster17-80x/rocksdb-fd ]; then
+	data_dir=cluster29/prismdb
+	if [ ! "$(grep "run-end-timestamp" $data_dir/info.json)" ]; then
+		echo "PrismDB crashes under cluster29. Using the last 10% of its completed run phase."
+		echo -e "\t\"run-end-timestamp(ns)\": $(tail -n 1 $data_dir/progress | cut -d' ' -f1)\n}" >> $data_dir/info.json
+	fi
+	~/tests/plot/twitter_ops.py .
+fi
---
+++ tools/save.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env sh
+if [ ! "$1" ]; then
+	echo Usage: $0 paper-dir
+	exit 1
+fi
+
+paper_dir=$1
+mkdir -p "$paper_dir"/fig
+mkdir -p "$paper_dir"/data
+if [ -f ycsb-sweep.pdf ]; then
+	cp ycsb-sweep.pdf "$paper_dir"/fig/
+	cp ycsb-sweep.tex "$paper_dir"/data/
+	cp ops-200B.pdf "$paper_dir"/fig/
+	cp overhead-uniform-rocksdb-tiered.tex "$paper_dir"/data/
+	cp latency.pdf "$paper_dir"/fig
+
+	cp cputime-breakdown-200B.pdf "$paper_dir"/fig/
+	cp cpu-breakdown-200B.tex "$paper_dir"/data/
+	cp io-breakdown-200B.pdf "$paper_dir"/fig/
+	cp io-breakdown-200B.tex "$paper_dir"/data/
+
+	cd read_0.75_insert_0.25_hotspot0.05_110GB_220GB
+	cp throughput-breakdown.pdf "$paper_dir"/fig/
+	cp table-no-hotness-aware-compaction.tex "$paper_dir"/data/
+	cd ..
+
+	cd ycsbc_uniform_110GB_220GB
+	cp table-promote-accessed.tex promote-accessed.tex "$paper_dir"/data/
+	cd ..
+
+	cp ops-1.1TB.pdf "$paper_dir"/fig/
+fi
+
+if [ -f u24685531/hotrap/plot/dynamic-workload.pdf ]; then
+	cp u24685531/hotrap/plot/dynamic-workload.pdf "$paper_dir"/fig/
+fi
+
+if [ -f twitter-speedup.pdf ]; then
+	cp twitter-speedup.pdf ~/sshfs/paper/fig/
+	cp twitter-speedup.tex ~/sshfs/paper/data/
+fi
+
+if [ -f twitter-ops.pdf ]; then
+	cp twitter-ops.pdf ~/sshfs/paper/fig/
+	cp twitter.tex ~/sshfs/paper/data/
+fi
