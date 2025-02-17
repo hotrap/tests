b0fa227e87304863d8fc9c5b292566b2ddbe9040
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-02-17 15:37:34 +0000

    block cache under 1.1TB workloads: hotrap 2GiB, others 3GiB

--- workloads/test-rocksdb-tiered-1.1TB.sh
+++ workloads/test-rocksdb-tiered-1.1TB.sh
@@ -5,6 +5,6 @@ if [ $# -lt 2 -o $# -gt 3 ]; then
 fi
 workload_file=$(realpath -s "$1")
 workspace=$(realpath "$(dirname $0)"/../..)
-$(dirname $0)/test-rocksdb-tiered-generic.sh 1.5GiB 100GB $((13 * 64))MiB "$2" \
+$(dirname $0)/test-rocksdb-tiered-generic.sh 3GiB 100GB $((13 * 64))MiB "$2" \
 	"LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4" \
 	"--enable_fast_generator --workload_file=$workload_file $3"
