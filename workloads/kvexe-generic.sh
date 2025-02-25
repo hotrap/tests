6c5dde32c63512f980a751bbb89f5f634e93719d
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-02-25 13:45:20 +0000

    Dump core with ASAN

--- workloads/kvexe-generic.sh
+++ workloads/kvexe-generic.sh
@@ -19,7 +19,7 @@ workspace=$(realpath ../..)
 ulimit -n 100000
 # Dump core when crash
 ulimit -c unlimited
-export ASAN_OPTIONS=abort_on_error=1
+export ASAN_OPTIONS="abort_on_error=1:disable_coredump=0"
 cd $DIR
 $workspace/tests/helper/exe-while.sh . sh -c "$command \
 --switches=0x1 \
