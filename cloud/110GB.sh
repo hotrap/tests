7cbeef823bb70d111d5e5509d45fe8775ebaf83a
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-05-04 16:25:07 +0800

    Clean up

--- cloud/110GB.sh
+++ cloud/110GB.sh
@@ -72,7 +72,6 @@ for workload in "${workloads[@]}"; do
 	cloud-run machine-config/110GB.json run-hotrap $workload hotrap
 done
 cloud-run machine-config/110GB.json run-workload "u24685531" hotrap
-cloud-run machine-config/110GB.json run-workload "2-4-6-8" hotrap
 cloud-run machine-config/110GB.json run-hotrap "ycsbc_uniform_110GB_220GB" promote-accessed
 cloud-run machine-config/110GB.json run-hotrap "read_0.75_insert_0.25_hotspot0.05_110GB_220GB" no-hotness-aware-compaction
 
