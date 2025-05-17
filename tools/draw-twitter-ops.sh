87a04e778a6ce099143644b2ab37e00ed8a48117
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2025-05-17 07:54:23 +0000

    Fix

--- tools/draw-twitter-ops.sh
+++ tools/draw-twitter-ops.sh
@@ -6,9 +6,10 @@ workspace=$(realpath $(dirname $0)/../..)
 tests=$workspace/tests
 
 data_dir=cluster29/prismdb
-if [ ! "$(grep "run-end-timestamp" $data_dir/info.json)" ]; then
-	echo "PrismDB crashes under cluster29. Using the last 10% of its completed run phase."
-	printf "\t\"run-end-timestamp(ns)\": $(tail -n 1 $data_dir/progress | cut -d' ' -f1)\n}" >> $data_dir/info.json
+if [ -f $data_dir/info.json ]; then
+	if [ ! "$(grep "run-end-timestamp" $data_dir/info.json)" ]; then
+		echo "PrismDB crashes under cluster29. Using the last 10% of its completed run phase."
+		printf "\t\"run-end-timestamp(ns)\": $(tail -n 1 $data_dir/progress | cut -d' ' -f1)\n}" >> $data_dir/info.json
+	fi
 fi
 $tests/plot/twitter_ops.py .
-
